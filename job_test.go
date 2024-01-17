95aad7c349a78b098cf21e2fa8d2dc483409a7d5
Blake Gentry <blakesgentry@gmail.com>
2024-01-17 10:01:13 -0600

    comprehensive tests for finalized_at constraint

--- job_test.go
+++ job_test.go
@@ -1,11 +1,18 @@
 package river
 
 import (
+	"context"
+	"fmt"
 	"testing"
 	"time"
 
+	"github.com/jackc/pgx/v5"
 	"github.com/stretchr/testify/require"
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
 
+	"github.com/riverqueue/river/internal/dbsqlc"
+	"github.com/riverqueue/river/internal/util/ptrutil"
 	"github.com/riverqueue/river/rivertype"
 )
 
@@ -32,4 +39,125 @@ func TestJobUniqueOpts_validate(t *testing.T) {
 
 	require.EqualError(t, (&UniqueOpts{ByPeriod: 1 * time.Millisecond}).validate(), "JobUniqueOpts.ByPeriod should not be less than 1 second")
 	require.EqualError(t, (&UniqueOpts{ByState: []rivertype.JobState{rivertype.JobState("invalid")}}).validate(), `JobUniqueOpts.ByState contains invalid state "invalid"`)
+}
+
+func TestJobFinalizedAtConstraint(t *testing.T) {
+	t.Parallel()
+
+	ctx := context.Background()
+
+	type testBundle struct {
+		queries *dbsqlc.Queries
+		tx      pgx.Tx
+	}
+
+	capitalizeJobState := func(state dbsqlc.JobState) string {
+		return cases.Title(language.English, cases.NoLower).String(string(state))
+	}
+
+	setup := func(t *testing.T) (*Client[pgx.Tx], *testBundle) {
+		t.Helper()
+
+		config := newTestConfig(t, nil)
+		client := newTestClient(ctx, t, config)
+
+		tx, err := client.driver.GetDBPool().Begin(ctx)
+		require.NoError(t, err)
+		t.Cleanup(func() { tx.Rollback(ctx) })
+
+		return client, &testBundle{
+			tx: tx,
+		}
+	}
+
+	for _, state := range []dbsqlc.JobState{
+		dbsqlc.JobStateCancelled,
+		dbsqlc.JobStateCompleted,
+		dbsqlc.JobStateDiscarded,
+	} {
+		state := state // capture range variable
+
+		t.Run(fmt.Sprintf("CannotSetState%sWithoutFinalizedAt", capitalizeJobState(state)), func(t *testing.T) {
+			t.Parallel()
+			client, bundle := setup(t)
+
+			insertedJob, err := client.Insert(ctx, noOpArgs{}, nil)
+			require.NoError(t, err)
+
+			// Try updating the job to the target state but without a finalized_at,
+			// expect an error:
+			_, err = bundle.queries.JobUpdate(ctx, bundle.tx, dbsqlc.JobUpdateParams{
+				ID:            insertedJob.ID,
+				State:         state,
+				StateDoUpdate: true,
+			})
+			require.ErrorContains(t, err, "violates check constraint \"finalized_or_finalized_at_null\"")
+		})
+
+		t.Run(fmt.Sprintf("CanSetState%sWithFinalizedAt", capitalizeJobState(state)), func(t *testing.T) {
+			t.Parallel()
+			client, bundle := setup(t)
+
+			insertedJob, err := client.Insert(ctx, noOpArgs{}, nil)
+			require.NoError(t, err)
+
+			// Try updating the job to the target state but without a finalized_at,
+			// expect an error:
+			jobAfter, err := bundle.queries.JobUpdate(ctx, bundle.tx, dbsqlc.JobUpdateParams{
+				ID:                  insertedJob.ID,
+				FinalizedAt:         ptrutil.Ptr(time.Now()),
+				FinalizedAtDoUpdate: true,
+				State:               state,
+				StateDoUpdate:       true,
+			})
+			require.NoError(t, err)
+			require.Equal(t, state, jobAfter.State)
+		})
+	}
+
+	for _, state := range []dbsqlc.JobState{
+		dbsqlc.JobStateAvailable,
+		dbsqlc.JobStateRetryable,
+		dbsqlc.JobStateRunning,
+		dbsqlc.JobStateScheduled,
+	} {
+		state := state // capture range variable
+
+		t.Run(fmt.Sprintf("CanSetState%sWithoutFinalizedAt", capitalizeJobState(state)), func(t *testing.T) {
+			t.Parallel()
+			client, bundle := setup(t)
+
+			insertedJob, err := client.Insert(ctx, noOpArgs{}, nil)
+			require.NoError(t, err)
+
+			// Try updating the job to the target state but without a finalized_at,
+			// expect an error:
+			jobAfter, err := bundle.queries.JobUpdate(ctx, bundle.tx, dbsqlc.JobUpdateParams{
+				ID:            insertedJob.ID,
+				State:         state,
+				StateDoUpdate: true,
+			})
+			require.NoError(t, err)
+			require.Equal(t, state, jobAfter.State)
+		})
+
+		t.Run(fmt.Sprintf("CannotSetState%sWithFinalizedAt", capitalizeJobState(state)), func(t *testing.T) {
+			t.Parallel()
+			client, bundle := setup(t)
+
+			insertedJob, err := client.Insert(ctx, noOpArgs{}, nil)
+			require.NoError(t, err)
+
+			// Try updating the job to the target state but without a finalized_at,
+			// expect an error:
+			_, err = bundle.queries.JobUpdate(ctx, bundle.tx, dbsqlc.JobUpdateParams{
+				ID:                  insertedJob.ID,
+				FinalizedAt:         ptrutil.Ptr(time.Now()),
+				FinalizedAtDoUpdate: true,
+				State:               state,
+				StateDoUpdate:       true,
+			})
+			require.ErrorContains(t, err, "violates check constraint \"finalized_or_finalized_at_null\"")
+		})
+	}
 }
