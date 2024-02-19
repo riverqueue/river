d6e2c3700d87f36d6c6edb1f706a3636c402925d
Brandur <brandur@brandur.org>
2024-02-19 11:53:55 -0800

    Reuse timer on `DebouncedChan` + put in some comments + continous test
    I wanted to understand a little more clearly how `DebouncedChan` works,
    so I went in to look for a few refactoring opportunities. There was a
    TODO in the file to potentially reuse the internal timer so it doesn't
    need to be reallocated every debounce cycle, so here we implement that.
    
    To make sure that the reused timer is really working we also add a new
    "continuous" test that checks that the channel will in fact fire many
    times in a row. We also go through and add a few comments and rename a
    couple things for clarity, and bring in a `TestMain` wrapper so that the
    package tests are checked for goroutine leaks.

--- internal/util/chanutil/debounced_chan_test.go
+++ internal/util/chanutil/debounced_chan_test.go
@@ -2,9 +2,12 @@ package chanutil
 
 import (
 	"context"
+	"math"
 	"sync"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestDebouncedChan_TriggersImmediately(t *testing.T) {
@@ -76,4 +79,64 @@ func TestDebouncedChan_OnlyBuffersOneEvent(t *testing.T) {
 		t.Fatal("received from debounced chan unexpectedly")
 	case <-time.After(20 * time.Millisecond):
 	}
+}
+
+func TestDebouncedChan_ContinuousOperation(t *testing.T) {
+	t.Parallel()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	const (
+		cooldown  = 17 * time.Millisecond
+		increment = 1 * time.Millisecond
+		testTime  = 150 * time.Millisecond
+	)
+
+	var (
+		debouncedChan = NewDebouncedChan(ctx, cooldown)
+		goroutineDone = make(chan struct{})
+		numSignals    int
+	)
+
+	go func() {
+		defer close(goroutineDone)
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case <-debouncedChan.C():
+				numSignals++
+			}
+		}
+	}()
+
+	for tm := increment; tm <= testTime; tm += increment {
+		time.Sleep(increment)
+		debouncedChan.Call()
+	}
+
+	cancel()
+
+	select {
+	case <-goroutineDone:
+	case <-time.After(3 * time.Second):
+		require.FailNow(t, "Timed out waiting for goroutine to finish")
+	}
+
+	// Expect number of signals equal to number of cooldown periods that fit
+	// into our total test time, multiplied by two, because the debounced chan
+	// fires at the beginning and end of a bounce period. +1 for the last period
+	// that fires on the leading edge, but which we don't give time for the
+	// timer to fully expire. (We've chosen numbers so that test time doesn't
+	// divide by cooldown perfectly.)
+	//
+	// This usually lands right on the expected number, but allow a delta of
+	// +/-4 to allow the channel to be off by two cycles (again, one cycle
+	// signals once at leading edge of the period and once at trailing, so 2
+	// cycles * 2 signals = 4) in either direction. By running at `-count 1000`
+	// or so I can usually reproduce an off-by-one-or-two cycle.
+	expectedNumSignal := int(math.Round(float64(testTime)/float64(cooldown)))*2 + 1
+	t.Logf("Expected: %d, actual: %d", expectedNumSignal, numSignals)
+	require.InDelta(t, expectedNumSignal, numSignals, 4)
 }
