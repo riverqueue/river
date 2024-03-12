3e7d87d2aab0e5e507d2d940967b398c27a64555
Brandur Leach <brandur@brandur.org>
2024-03-11 20:01:18 -0700

    Refactor elector somewhat + test suite + poll-only mode (#263)
    Here, do a little refactoring in the elector. The overall code doesn't
    change that much, but we try to tighten things up with little things
    like improved logging and real exponential backoff. It becomes a
    start/stop service so that it's a little more normalized with other code
    and more robust on start/stop.
    
    The major improvement is the addition of a test suite. Although there
    was a nominal suite before that was added during the driver refactor to
    test the `attemptElectOrReelect` function, the elector was only tested
    indirectly otherwise through the client. The change comes with a variety
    of tests that exercise the elector's various behaviors, including one
    that pits multiple competing electors against each other to make sure
    that works.
    
    Lastly, the elector gains a "poll only" mode in which we check that it
    can still function using polling only in case a listener isn't
    available. This doesn't have any effect on River feature-wise yet, but
    the idea is that after we've added a similar capability to the producer,
    we'll be able to support systems where `LISTEN`/`NOTIFY` aren't
    available like PgBouncer in transaction mode, or possibly even MySQL or
    SQLite in the future. We send the poll only mode through the same
    barrage of tests that we require it to pass when using a database pool.

--- internal/leadership/elector_test.go
+++ internal/leadership/elector_test.go
@@ -2,17 +2,319 @@ package leadership
 
 import (
 	"context"
+	"log/slog"
 	"testing"
 	"time"
 
+	"github.com/jackc/pgx/v5"
 	"github.com/stretchr/testify/require"
 
+	"github.com/riverqueue/river/internal/baseservice"
+	"github.com/riverqueue/river/internal/componentstatus"
+	"github.com/riverqueue/river/internal/notifier"
 	"github.com/riverqueue/river/internal/riverinternaltest"
+	"github.com/riverqueue/river/internal/riverinternaltest/sharedtx"
+	"github.com/riverqueue/river/internal/riverinternaltest/startstoptest"
 	"github.com/riverqueue/river/internal/riverinternaltest/testfactory"
 	"github.com/riverqueue/river/internal/util/ptrutil"
 	"github.com/riverqueue/river/riverdriver"
 	"github.com/riverqueue/river/riverdriver/riverpgxv5"
+	"github.com/riverqueue/river/rivertype"
 )
+
+const defaultInstanceName = "default"
+
+func TestElector_PollOnly(t *testing.T) {
+	t.Parallel()
+
+	var (
+		ctx    = context.Background()
+		driver = riverpgxv5.New(nil)
+	)
+
+	type electorBundle struct {
+		tx pgx.Tx
+	}
+
+	testElector(ctx, t,
+		func(t *testing.T) *electorBundle {
+			t.Helper()
+
+			tx := riverinternaltest.TestTx(ctx, t)
+
+			// We'll put multiple electors on one transaction. Make sure they can
+			// live with each other in relative harmony.
+			tx = sharedtx.NewSharedTx(tx)
+
+			return &electorBundle{
+				tx: tx,
+			}
+		},
+		func(t *testing.T, electorBundle *electorBundle) *Elector {
+			t.Helper()
+
+			return NewElector(
+				riverinternaltest.BaseServiceArchetype(t),
+				driver.UnwrapExecutor(electorBundle.tx),
+				nil,
+				defaultInstanceName,
+				"test-client-id",
+			)
+		})
+}
+
+func TestElector_WithNotifier(t *testing.T) {
+	t.Parallel()
+
+	ctx := context.Background()
+
+	type electorBundle struct {
+		archetype *baseservice.Archetype
+		exec      riverdriver.Executor
+		notifier  *notifier.Notifier
+	}
+
+	testElector(ctx, t,
+		func(t *testing.T) *electorBundle {
+			t.Helper()
+
+			var (
+				archetype = riverinternaltest.BaseServiceArchetype(t)
+				dbPool    = riverinternaltest.TestDB(ctx, t)
+				driver    = riverpgxv5.New(dbPool)
+			)
+
+			notifier := notifier.New(archetype, driver.GetListener(), func(s componentstatus.Status) {})
+			{
+				require.NoError(t, notifier.Start(ctx))
+				t.Cleanup(notifier.Stop)
+			}
+
+			return &electorBundle{
+				archetype: archetype,
+				exec:      driver.GetExecutor(),
+				notifier:  notifier,
+			}
+		},
+		func(t *testing.T, electorBundle *electorBundle) *Elector {
+			t.Helper()
+
+			return NewElector(
+				electorBundle.archetype,
+				electorBundle.exec,
+				electorBundle.notifier,
+				defaultInstanceName,
+				"test-client-id",
+			)
+		})
+}
+
+// This system of "elector bundles" may appear to be a little convoluted, but
+// it's built so that we can initialize multiple electors against a single
+// database or transaction.
+func testElector[TElectorBundle any](
+	ctx context.Context,
+	t *testing.T,
+	makeElectorBundle func(t *testing.T) TElectorBundle,
+	makeElector func(t *testing.T, bundle TElectorBundle) *Elector,
+) {
+	t.Helper()
+
+	type testBundle struct {
+		electorBundle TElectorBundle
+		exec          riverdriver.Executor
+	}
+
+	setup := func(t *testing.T) (*Elector, *testBundle) {
+		t.Helper()
+
+		electorBundle := makeElectorBundle(t)
+
+		elector := makeElector(t, electorBundle)
+		elector.testSignals.Init()
+
+		return elector, &testBundle{
+			electorBundle: electorBundle,
+			exec:          elector.exec,
+		}
+	}
+
+	startElector := func(ctx context.Context, t *testing.T, elector *Elector) {
+		t.Helper()
+		t.Logf("Starting " + elector.clientID)
+		require.NoError(t, elector.Start(ctx))
+		t.Cleanup(elector.Stop)
+	}
+
+	t.Run("StartsGainsLeadershipAndStops", func(t *testing.T) {
+		t.Parallel()
+
+		elector, bundle := setup(t)
+
+		startElector(ctx, t, elector)
+
+		elector.testSignals.GainedLeadership.WaitOrTimeout()
+
+		leader, err := bundle.exec.LeaderGetElectedLeader(ctx, defaultInstanceName)
+		require.NoError(t, err)
+		require.Equal(t, elector.clientID, leader.LeaderID)
+
+		elector.Stop()
+
+		elector.testSignals.ResignedLeadership.WaitOrTimeout()
+
+		_, err = bundle.exec.LeaderGetElectedLeader(ctx, defaultInstanceName)
+		require.ErrorIs(t, err, rivertype.ErrNotFound)
+	})
+
+	t.Run("NotifiesSubscribers", func(t *testing.T) {
+		t.Parallel()
+
+		elector, _ := setup(t)
+
+		sub := elector.Listen()
+		t.Cleanup(func() { elector.unlisten(sub) })
+
+		// Drain an initial notification that occurs on Listen.
+		notification := riverinternaltest.WaitOrTimeout(t, sub.ch)
+		require.False(t, notification.IsLeader)
+
+		startElector(ctx, t, elector)
+
+		elector.testSignals.GainedLeadership.WaitOrTimeout()
+
+		notification = riverinternaltest.WaitOrTimeout(t, sub.ch)
+		require.True(t, notification.IsLeader)
+
+		elector.Stop()
+
+		elector.testSignals.ResignedLeadership.WaitOrTimeout()
+
+		notification = riverinternaltest.WaitOrTimeout(t, sub.ch)
+		require.False(t, notification.IsLeader)
+	})
+
+	t.Run("SustainsLeadership", func(t *testing.T) {
+		t.Parallel()
+
+		elector, _ := setup(t)
+
+		startElector(ctx, t, elector)
+
+		elector.testSignals.GainedLeadership.WaitOrTimeout()
+
+		// The leadership maintenance loop also listens on the leadership
+		// notification channel. Take advantage of that to cause an
+		// immediate reelect attempt with no sleep.
+		elector.leadershipNotificationChan <- struct{}{}
+		elector.testSignals.MaintainedLeadership.WaitOrTimeout()
+
+		elector.leadershipNotificationChan <- struct{}{}
+		elector.testSignals.MaintainedLeadership.WaitOrTimeout()
+
+		elector.leadershipNotificationChan <- struct{}{}
+		elector.testSignals.MaintainedLeadership.WaitOrTimeout()
+
+		elector.Stop()
+
+		elector.testSignals.ResignedLeadership.WaitOrTimeout()
+	})
+
+	t.Run("LosesLeadership", func(t *testing.T) {
+		t.Parallel()
+
+		elector, bundle := setup(t)
+
+		startElector(ctx, t, elector)
+
+		elector.testSignals.GainedLeadership.WaitOrTimeout()
+
+		t.Logf("Force resigning " + elector.clientID)
+
+		// Artificially force resign the elector and add a new leader record
+		// so that it can't be elected again.
+		_, err := bundle.exec.LeaderResign(ctx, &riverdriver.LeaderResignParams{
+			LeaderID:        elector.clientID,
+			LeadershipTopic: string(notifier.NotificationTopicLeadership),
+			Name:            defaultInstanceName,
+		})
+		require.NoError(t, err)
+
+		_ = testfactory.Leader(ctx, t, bundle.exec, &testfactory.LeaderOpts{
+			LeaderID: ptrutil.Ptr("other-client-id"),
+		})
+
+		elector.leadershipNotificationChan <- struct{}{}
+		elector.testSignals.LostLeadership.WaitOrTimeout()
+
+		// Wait for the elector to try and fail to gain leadership so we
+		// don't finish the test while it's still operating.
+		elector.testSignals.DeniedLeadership.WaitOrTimeout()
+
+		elector.Stop()
+	})
+
+	t.Run("CompetingElectors", func(t *testing.T) {
+		t.Parallel()
+
+		elector1, bundle := setup(t)
+		elector1.clientID = "elector1"
+
+		{
+			startElector(ctx, t, elector1)
+
+			// next to avoid any raciness.
+			t.Logf("Waiting for %s to gain leadership", elector1.clientID)
+			elector1.testSignals.GainedLeadership.WaitOrTimeout()
+
+			leader, err := bundle.exec.LeaderGetElectedLeader(ctx, defaultInstanceName)
+			require.NoError(t, err)
+			require.Equal(t, elector1.clientID, leader.LeaderID)
+		}
+
+		// Make another elector and make sure it's using the same executor.
+		elector2 := makeElector(t, bundle.electorBundle)
+		elector2.clientID = "elector2"
+		elector2.exec = elector1.exec
+		elector2.testSignals.Init()
+
+		{
+			startElector(ctx, t, elector2)
+
+			elector2.testSignals.DeniedLeadership.WaitOrTimeout()
+
+			t.Logf("Stopping " + elector1.clientID)
+			elector1.Stop()
+			elector1.testSignals.ResignedLeadership.WaitOrTimeout()
+
+			// Cheat if we're in poll only by notifying leadership channel to
+			// wake the elector from sleep.
+			if elector2.notifier == nil {
+				elector2.leadershipNotificationChan <- struct{}{}
+			}
+
+			t.Logf("Waiting for %s to gain leadership", elector2.clientID)
+			elector2.testSignals.GainedLeadership.WaitOrTimeout()
+
+			t.Logf("Stopping " + elector2.clientID)
+			elector2.Stop()
+			elector2.testSignals.ResignedLeadership.WaitOrTimeout()
+		}
+
+		_, err := bundle.exec.LeaderGetElectedLeader(ctx, defaultInstanceName)
+		require.ErrorIs(t, err, rivertype.ErrNotFound)
+	})
+
+	t.Run("StartStopStress", func(t *testing.T) {
+		t.Parallel()
+
+		elector, _ := setup(t)
+		elector.Logger = riverinternaltest.LoggerWarn(t) // loop started/stop log is very noisy; suppress
+		elector.testSignals = electorTestSignals{}       // deinit so channels don't fill
+
+		startstoptest.Stress(ctx, t, elector)
+	})
+}
 
 func TestAttemptElectOrReelect(t *testing.T) {
 	t.Parallel()
@@ -26,7 +328,8 @@ func TestAttemptElectOrReelect(t *testing.T) {
 	ctx := context.Background()
 
 	type testBundle struct {
-		exec riverdriver.Executor
+		exec   riverdriver.Executor
+		logger *slog.Logger
 	}
 
 	setup := func(t *testing.T) *testBundle {
@@ -35,7 +338,8 @@ func TestAttemptElectOrReelect(t *testing.T) {
 		driver := riverpgxv5.New(nil)
 
 		return &testBundle{
-			exec: driver.UnwrapExecutor(riverinternaltest.TestTx(ctx, t)),
+			exec:   driver.UnwrapExecutor(riverinternaltest.TestTx(ctx, t)),
+			logger: riverinternaltest.Logger(t),
 		}
 	}
 
