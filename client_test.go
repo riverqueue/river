riverqueue/river

commit a939d340ff1ca377a0562ee208f32fe1a17212af

Make client a start/stop service (#272)
Here, continue service refactoring to its ultimate conclusion, and make
the client itself a start/stop service, having a couple advantages:

* (IMO) considerably simplifies the start and stop code, putting it all
  in one place, and largely even one function. Fewer helpers to follow
  around to understand what's going on.

* Makes the client behave gracefully on double starts/stops.

* Allows the client to easily handle start/stop under duress. Any number
  of goroutines can be starting or stopping it simultaneously and it's
  guaranteed free from races.

Because the client's stop functions are different from the signature of
other start/stop services (taking a context and returning an error), I
had to modify the base start/stop service somewhat and allow for an
additional `StopInit` helper (like `StartInit`) that lets stop behavior
be customized while still providing race-free operations.

client.go
@@ -23,6 +23,7 @@ import (
 	"github.com/riverqueue/river/internal/maintenance/startstop"
 	"github.com/riverqueue/river/internal/notifier"
 	"github.com/riverqueue/river/internal/rivercommon"
+	"github.com/riverqueue/river/internal/util/maputil"
 	"github.com/riverqueue/river/internal/util/randutil"
 	"github.com/riverqueue/river/internal/util/sliceutil"
 	"github.com/riverqueue/river/internal/util/valutil"
@@ -262,19 +263,15 @@ type QueueConfig struct {
 // multiple instances operating on different databases or Postgres schemas
 // within a single database.
 type Client[TTx any] struct {
-	// BaseService can't be embedded like on other services because its
-	// properties would leak to the external API.
-	baseService baseservice.BaseService
+	// BaseService and BaseStartStop can't be embedded like on other services
+	// because their properties would leak to the external API.
+	baseService   baseservice.BaseService
+	baseStartStop startstop.BaseStartStop
 
 	completer jobcompleter.JobCompleter
 	config    *Config
 	driver    riverdriver.Driver[TTx]
 	elector   *leadership.Elector
-
-	// fetchWorkCancel cancels the context used for fetching new work. This
-	// will be used to stop fetching new work whenever stop is initiated, or
-	// when the context provided to Run is itself cancelled.
-	fetchWorkCancel context.CancelCauseFunc
 
 	monitor              *clientMonitor
 	notifier             *notifier.Notifier
@@ -284,10 +281,10 @@ type Client[TTx any] struct {
 	subscriptions        map[int]*eventSubscription
 	subscriptionsMu      sync.Mutex
 	subscriptionsSeq     int // used for generating simple IDs
-	stopComplete         chan struct{}
 	statsAggregate       jobstats.JobStatistics
 	statsMu              sync.Mutex
 	statsNumJobs         int
+	stopped              chan struct{}
 	testSignals          clientTestSignals
 	uniqueInserter       *dbunique.UniqueInserter
 
@@ -433,7 +430,6 @@ func NewClient[TTx any](driver riverdriver.Driver[TTx], config *Config) (*Client
 		driver:               driver,
 		monitor:              newClientMonitor(),
 		producersByQueueName: make(map[string]*producer),
-		stopComplete:         make(chan struct{}),
 		subscriptions:        make(map[int]*eventSubscription),
 		testSignals:          clientTestSignals{},
 		uniqueInserter: baseservice.Init(archetype, &dbunique.UniqueInserter{
@@ -575,146 +571,154 @@ func NewClient[TTx any](driver riverdriver.Driver[TTx], config *Config) (*Client
 // jobs, but will also cancel the context for any currently-running jobs. If
 // using StopAndCancel, there's no need to also call Stop.
 func (c *Client[TTx]) Start(ctx context.Context) error {
-	if !c.config.willExecuteJobs() {
-		return errors.New("client Queues and Workers must be configured for a client to start working")
-	}
-	if c.config.Workers != nil && len(c.config.Workers.workersMap) < 1 {
-		return errors.New("at least one Worker must be added to the Workers bundle")
+	fetchCtx, shouldStart, stopped := c.baseStartStop.StartInit(ctx)
+	if !shouldStart {
+		return nil
 	}
 
-	// Before doing anything else, make an initial connection to the database to
-	// verify that it appears healthy. Many of the subcomponents below start up
-	// in a goroutine and in case of initial failure, only produce a log line,
-	// so even in the case of a fundamental failure like the database not being
-	// available, the client appears to have started even though it's completely
-	// non-functional. Here we try to make an initial assessment of health and
-	// return quickly in case of an apparent problem.
-	_, err := c.driver.GetExecutor().Exec(ctx, "SELECT 1")
-	if err != nil {
-		return fmt.Errorf("error making initial connection to database: %w", err)
+	c.stopped = stopped
+
+	stopProducers := func() {
+		startstop.StopAllParallel(sliceutil.Map(
+			maputil.Values(c.producersByQueueName),
+			func(p *producer) startstop.Service { return p }),
+		)
 	}
 
-	// In case of error, stop any services that might have started. This
-	// is safe because even services that were never started will still
-	// tolerate being stopped.
-	stopServicesOnError := func() {
-		startstop.StopAllParallel(c.services)
-		c.monitor.Stop()
-	}
+	var workCtx context.Context
 
-	// Monitor should be the first subprocess to start, and the last to stop.
-	// It's not part of the waitgroup because we need to wait for everything else
-	// to shut down prior to closing the monitor.
-	//
-	// Unlike other services, it's given a background context so that it doesn't
-	// cancel on normal stops.
-	if err := c.monitor.Start(context.Background()); err != nil { //nolint:contextcheck
+	// Startup code. Wrapped in a closure so it doesn't have to remember to
+	// close the stopped channel if returning with an error.
+	if err := func() error {
+		if !c.config.willExecuteJobs() {
+			return errors.New("client Queues and Workers must be configured for a client to start working")
+		}
+		if c.config.Workers != nil && len(c.config.Workers.workersMap) < 1 {
+			return errors.New("at least one Worker must be added to the Workers bundle")
+		}
+
+		// Before doing anything else, make an initial connection to the database to
+		// verify that it appears healthy. Many of the subcomponents below start up
+		// in a goroutine and in case of initial failure, only produce a log line,
+		// so even in the case of a fundamental failure like the database not being
+		// available, the client appears to have started even though it's completely
+		// non-functional. Here we try to make an initial assessment of health and
+		// return quickly in case of an apparent problem.
+		_, err := c.driver.GetExecutor().Exec(fetchCtx, "SELECT 1")
+		if err != nil {
+			return fmt.Errorf("error making initial connection to database: %w", err)
+		}
+
+		// In case of error, stop any services that might have started. This
+		// is safe because even services that were never started will still
+		// tolerate being stopped.
+		stopServicesOnError := func() {
+			startstop.StopAllParallel(c.services)
+			c.monitor.Stop()
+		}
+
+		// Monitor should be the first subprocess to start, and the last to stop.
+		// It's not part of the waitgroup because we need to wait for everything else
+		// to shut down prior to closing the monitor.
+		//
+		// Unlike other services, it's given a background context so that it doesn't
+		// cancel on normal stops.
+		if err := c.monitor.Start(context.Background()); err != nil { //nolint:contextcheck
+			return err
+		}
+
+		if c.completer != nil {
+			// The completer is part of the services list below, but although it can
+			// stop gracefully along with all the other services, it needs to be
+			// started with a context that's _not_ fetchCtx. This ensures that even
+			// when fetch is cancelled on shutdown, the completer is still given a
+			// separate opportunity to start stopping only after the producers have
+			// finished up and returned.
+			if err := c.completer.Start(ctx); err != nil {
+				stopServicesOnError()
+				return err
+			}
+
+			// Receives job complete notifications from the completer and
+			// distributes them to any subscriptions.
+			c.completer.Subscribe(c.distributeJobCompleterCallback)
+		}
+
+		// We use separate contexts for fetching and working to allow for a graceful
+		// stop. Both inherit from the provided context, so if it's cancelled, a
+		// more aggressive stop will be initiated.
+		workCtx, c.workCancel = context.WithCancelCause(withClient[TTx](ctx, c))
+
+		for _, service := range c.services {
+			if err := service.Start(fetchCtx); err != nil {
+				stopServicesOnError()
+				return err
+			}
+		}
+
+		for _, producer := range c.producersByQueueName {
+			producer := producer
+
+			if err := producer.StartWorkContext(fetchCtx, workCtx); err != nil {
+				stopProducers()
+				stopServicesOnError()
+				return err
+			}
+		}
+
+		return nil
+	}(); err != nil {
+		defer close(stopped)
+		if errors.Is(context.Cause(fetchCtx), startstop.ErrStop) {
+			return rivercommon.ErrShutdown
+		}
 		return err
 	}
 
-	if c.completer != nil {
-		// The completer is part of the services list below, but although it can
-		// stop gracefully along with all the other services, it needs to be
-		// started with a context that's _not_ fetchCtx. This ensures that even
-		// when fetch is cancelled on shutdown, the completer is still given a
-		// separate opportunity to start stopping only after the producers have
-		// finished up and returned.
-		if err := c.completer.Start(ctx); err != nil {
-			stopServicesOnError()
-			return err
-		}
-
-		// Receives job complete notifications from the completer and
-		// distributes them to any subscriptions.
-		c.completer.Subscribe(c.distributeJobCompleterCallback)
-	}
-
-	// We use separate contexts for fetching and working to allow for a graceful
-	// stop. However, both inherit from the provided context so if it is
-	// cancelled a more aggressive stop will be initiated.
-	fetchCtx, fetchWorkCancel := context.WithCancelCause(ctx)
-	c.fetchWorkCancel = fetchWorkCancel
-	workCtx, workCancel := context.WithCancelCause(withClient[TTx](ctx, c))
-	c.workCancel = workCancel
-
-	for _, service := range c.services {
-		// TODO(brandur): Reevaluate the use of fetchCtx here. It's currently
-		// necessary to speed up shutdown so that all services start shutting
-		// down before having to wait for the producers to finish, but as
-		// stopping becomes more normalized (hopefully by making the client
-		// itself a start/stop service), we can likely accomplish that in a
-		// cleaner way.
-		if err := service.Start(fetchCtx); err != nil {
-			stopServicesOnError()
-			if errors.Is(context.Cause(ctx), rivercommon.ErrShutdown) {
-				return nil
-			}
-			return err
-		}
-	}
-
-	for _, producer := range c.producersByQueueName {
-		producer := producer
-
-		if err := producer.StartWorkContext(fetchCtx, workCtx); err != nil {
-			return err
-		}
-	}
-
 	go func() {
-		<-fetchCtx.Done()
-		c.signalStopComplete(ctx)
-	}()
+		defer close(stopped)
 
-	c.baseService.Logger.InfoContext(workCtx, "River client successfully started", slog.String("client_id", c.ID()))
+		c.baseService.Logger.InfoContext(ctx, "River client started", slog.String("client_id", c.ID()))
+		defer c.baseService.Logger.InfoContext(ctx, "River client stopped", slog.String("client_id", c.ID()))
+
+		// The call to Stop cancels this context. Block here until shutdown.
+		<-fetchCtx.Done()
+
+		// On stop, have the producers stop fetching first of all.
+		stopProducers()
+
+		// Stop all mainline services where stop order isn't important.
+		startstop.StopAllParallel(append(
+			// This list of services contains the completer, which should always
+			// stop after the producers so that any remaining work that was enqueued
+			// will have a chance to have its state completed as it finishes.
+			//
+			// TODO: there's a risk here that the completer is stuck on a job that
+			// won't complete. We probably need a timeout or way to move on in those
+			// cases.
+			c.services,
+
+			// Will only be started if this client was leader, but can tolerate a
+			// stop without having been started.
+			c.queueMaintainer,
+		))
+
+		// Remove all subscriptions and close corresponding channels.
+		func() {
+			c.subscriptionsMu.Lock()
+			defer c.subscriptionsMu.Unlock()
+
+			for subID, sub := range c.subscriptions {
+				close(sub.Chan)
+				delete(c.subscriptions, subID)
+			}
+		}()
+
+		// Shut down the monitor last so it can broadcast final status updates:
+		c.monitor.Stop()
+	}()
 
 	return nil
-}
-
-// ctx is used only for logging, not for lifecycle.
-func (c *Client[TTx]) signalStopComplete(ctx context.Context) {
-	for _, producer := range c.producersByQueueName {
-		producer.Stop()
-	}
-
-	// Stop all mainline services where stop order isn't important.
-	startstop.StopAllParallel(append(
-		// This list of services contains the completer, which should always
-		// stop after the producers so that any remaining work that was enqueued
-		// will have a chance to have its state completed as it finishes.
-		//
-		// TODO: there's a risk here that the completer is stuck on a job that
-		// won't complete. We probably need a timeout or way to move on in those
-		// cases.
-		c.services,
-
-		// Will only be started if this client was leader, but can tolerate a stop
-		// without having been started.
-		c.queueMaintainer,
-	))
-
-	c.baseService.Logger.InfoContext(ctx, c.baseService.Name+": All services stopped")
-
-	// As of now, the Adapter doesn't have any async behavior, so we don't need
-	// to wait for it to stop.  Once all executors and completers are done, we
-	// know that nothing else is happening that's from us.
-
-	// Remove all subscriptions and close corresponding channels.
-	func() {
-		c.subscriptionsMu.Lock()
-		defer c.subscriptionsMu.Unlock()
-
-		for subID, sub := range c.subscriptions {
-			close(sub.Chan)
-			delete(c.subscriptions, subID)
-		}
-	}()
-
-	// Shut down the monitor last so it can broadcast final status updates:
-	c.monitor.Stop()
-
-	c.baseService.Logger.InfoContext(ctx, c.baseService.Name+": Stop complete")
-	close(c.stopComplete)
 }
 
 // Stop performs a graceful shutdown of the Client. It signals all producers
@@ -725,20 +729,17 @@ func (c *Client[TTx]) signalStopComplete(ctx context.Context) {
 // There's no need to call this method if a hard stop has already been initiated
 // by cancelling the context passed to Start or by calling StopAndCancel.
 func (c *Client[TTx]) Stop(ctx context.Context) error {
-	if c.fetchWorkCancel == nil {
-		return errors.New("client not started")
+	shouldStop, stopped, finalizeStop := c.baseStartStop.StopInit()
+	if !shouldStop {
+		return nil
 	}
 
-	c.baseService.Logger.InfoContext(ctx, c.baseService.Name+": Stop started")
-	c.fetchWorkCancel(rivercommon.ErrShutdown)
-	return c.awaitStop(ctx)
-}
-
-func (c *Client[TTx]) awaitStop(ctx context.Context) error {
 	select {
-	case <-ctx.Done():
+	case <-ctx.Done(): // stop context cancelled
+		finalizeStop(false) // not stopped; allow Stop to be called again
 		return ctx.Err()
-	case <-c.stopComplete:
+	case <-stopped:
+		finalizeStop(true)
 		return nil
 	}
 }
@@ -754,10 +755,22 @@ func (c *Client[TTx]) awaitStop(ctx context.Context) error {
 // no need to call this method if the context passed to Run is cancelled
 // instead.
 func (c *Client[TTx]) StopAndCancel(ctx context.Context) error {
+	shouldStop, stopped, finalizeStop := c.baseStartStop.StopInit()
+	if !shouldStop {
+		return nil
+	}
+
 	c.baseService.Logger.InfoContext(ctx, c.baseService.Name+": Hard stop started; cancelling all work")
-	c.fetchWorkCancel(rivercommon.ErrShutdown)
 	c.workCancel(rivercommon.ErrShutdown)
-	return c.awaitStop(ctx)
+
+	select {
+	case <-ctx.Done(): // stop context cancelled
+		finalizeStop(false) // not stopped; allow Stop to be called again
+		return ctx.Err()
+	case <-stopped:
+		finalizeStop(true)
+		return nil
+	}
 }
 
 // Stopped returns a channel that will be closed when the Client has stopped.
@@ -765,7 +778,7 @@ func (c *Client[TTx]) StopAndCancel(ctx context.Context) error {
 //
 // It is not affected by any contexts passed to Stop or StopAndCancel.
 func (c *Client[TTx]) Stopped() <-chan struct{} {
-	return c.stopComplete
+	return c.stopped
 }
 
 // Subscribe subscribes to the provided kinds of events that occur within the

client_test.go
@@ -24,6 +24,7 @@ import (
 	"github.com/riverqueue/river/internal/maintenance"
 	"github.com/riverqueue/river/internal/rivercommon"
 	"github.com/riverqueue/river/internal/riverinternaltest"
+	"github.com/riverqueue/river/internal/riverinternaltest/startstoptest"
 	"github.com/riverqueue/river/internal/riverinternaltest/testfactory"
 	"github.com/riverqueue/river/internal/util/dbutil"
 	"github.com/riverqueue/river/internal/util/ptrutil"
@@ -114,6 +115,16 @@ type callbackWorker struct {
 
 func (w *callbackWorker) Work(ctx context.Context, job *Job[callbackArgs]) error {
 	return w.fn(ctx, job)
+}
+
+// A small wrapper around Client that gives us a struct that corrects the
+// client's Stop function so that it can implement startstop.Service.
+type clientWithSimpleStop[TTx any] struct {
+	*Client[TTx]
+}
+
+func (c *clientWithSimpleStop[TTx]) Stop() {
+	_ = c.Client.Stop(context.Background())
 }
 
 func newTestConfig(t *testing.T, callback callbackFunc) *Config {
@@ -433,6 +444,16 @@ func Test_Client(t *testing.T) {
 		require.Equal(t, `relation "river_job" does not exist`, pgErr.Message)
 	})
 
+	t.Run("StartStopStress", func(t *testing.T) {
+		t.Parallel()
+
+		client, _ := setup(t)
+
+		clientWithStop := &clientWithSimpleStop[pgx.Tx]{Client: client}
+
+		startstoptest.StressErr(ctx, t, clientWithStop, rivercommon.ErrShutdown)
+	})
+
 	t.Run("StopAndCancel", func(t *testing.T) {
 		t.Parallel()
 
@@ -514,17 +535,6 @@ func Test_Client_Stop(t *testing.T) {
 			wg.Wait()
 		}
 	}
-
-	t.Run("not started", func(t *testing.T) {
-		t.Parallel()
-
-		dbPool := riverinternaltest.TestDB(ctx, t)
-		client := newTestClient(t, dbPool, newTestConfig(t, nil))
-
-		err := client.Stop(ctx)
-		require.Error(t, err)
-		require.Equal(t, "client not started", err.Error())
-	})
 
 	t.Run("no jobs in progress", func(t *testing.T) {
 		t.Parallel()

internal/maintenance/startstop/start_stop.go
@@ -2,8 +2,14 @@ package startstop
 
 import (
 	"context"
+	"errors"
 	"sync"
 )
+
+// ErrStop is an error injected into WithCancelCause when context is canceled
+// because a service is stopping. Makes it possible to differentiate a
+// controlled stop from a context cancellation.
+var ErrStop = errors.New("service stopped")
 
 // Service is a generalized interface for a service that starts and stops,
 // usually one backed by embedding BaseStartStop.
@@ -48,7 +54,7 @@ type serviceWithStopped interface {
 // A Stop implementation is provided automatically and it's not necessary to
 // override it.
 type BaseStartStop struct {
-	cancelFunc context.CancelFunc
+	cancelFunc context.CancelCauseFunc
 	mu         sync.Mutex
 	started    bool
 	stopped    chan struct{}
@@ -69,6 +75,21 @@ type BaseStartStop struct {
 //	     defer close(stopped)
 //
 //	     ...
+//
+// Be careful to also close it in the event of startup errors, otherwise a
+// service that failed to start once will never be able to start up.
+//
+//	ctx, shouldStart, stopped := s.StartInit(ctx)
+//	if !shouldStart {
+//	    return nil
+//	}
+//
+//	if err := possibleStartUpError(); err != nil {
+//	    close(stopped)
+//	    return err
+//	}
+//
+//	...
 func (s *BaseStartStop) StartInit(ctx context.Context) (context.Context, bool, chan struct{}) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
@@ -79,7 +100,7 @@ func (s *BaseStartStop) StartInit(ctx context.Context) (context.Context, bool, c
 
 	s.started = true
 	s.stopped = make(chan struct{})
-	ctx, s.cancelFunc = context.WithCancel(ctx)
+	ctx, s.cancelFunc = context.WithCancelCause(ctx)
 
 	return ctx, true, s.stopped
 }
@@ -87,19 +108,56 @@ func (s *BaseStartStop) StartInit(ctx context.Context) (context.Context, bool, c
 // Stop is an automatically provided implementation for the maintenance Service
 // interface's Stop.
 func (s *BaseStartStop) Stop() {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-
-	// Tolerate being told to stop without having been started.
-	if s.stopped == nil {
+	shouldStop, stopped, finalizeStop := s.StopInit()
+	if !shouldStop {
 		return
 	}
 
-	s.cancelFunc()
+	<-stopped
+	finalizeStop(true)
+}
 
-	<-s.stopped
-	s.started = false
-	s.stopped = nil
+// StopInit provides a way to build a more customized Stop implementation. It
+// should be avoided unless there'a an exceptional reason not to because Stop
+// should be fine in the vast majority of situations.
+//
+// It returns a boolean indicating whether the service should do any additional
+// work to stop (false is returned if the service was never started), a stopped
+// channel to wait on for full stop, and a finalizeStop function that should be
+// deferred in the stop function to ensure that locks are cleaned up and the
+// struct is reset after stopping.
+//
+//	shouldStop, stopped, finalizeStop := s.StartInit(ctx)
+//	if !shouldStop {
+//	    return
+//	}
+//
+//	defer finalizeStop(true)
+//
+//	...
+//
+// finalizeStop takes a boolean which indicates where the service should indeed
+// be considered stopped. This should usually be true, but callers can pass
+// false to cancel the stop action, keeping the service from starting again, and
+// potentially allowing the service to try another stop.
+func (s *BaseStartStop) StopInit() (bool, <-chan struct{}, func(didStop bool)) {
+	s.mu.Lock()
+
+	// Tolerate being told to stop without having been started.
+	if s.stopped == nil {
+		s.mu.Unlock()
+		return false, nil, func(didStop bool) {}
+	}
+
+	s.cancelFunc(ErrStop)
+
+	return true, s.stopped, func(didStop bool) {
+		defer s.mu.Unlock()
+		if didStop {
+			s.started = false
+			s.stopped = nil
+		}
+	}
 }
 
 // Stopped returns a channel that can be waited on for the service to be

internal/maintenance/startstop/start_stop_test.go
@@ -49,6 +49,15 @@ func testService(t *testing.T, newService func(t *testing.T) serviceWithStopped)
 		return newService(t), &testBundle{}
 	}
 
+	t.Run("StopAndStart", func(t *testing.T) {
+		t.Parallel()
+
+		service, _ := setup(t)
+
+		require.NoError(t, service.Start(ctx))
+		service.Stop()
+	})
+
 	t.Run("DoubleStop", func(t *testing.T) {
 		t.Parallel()
 
@@ -138,6 +147,119 @@ func TestBaseStartStopFunc(t *testing.T) {
 	}
 
 	testService(t, makeFunc)
+}
+
+func TestErrStop(t *testing.T) {
+	t.Parallel()
+
+	var (
+		workCtx context.Context
+		started = make(chan struct{})
+	)
+
+	startStop := StartStopFunc(func(ctx context.Context, shouldStart bool, stopped chan struct{}) error {
+		if !shouldStart {
+			return nil
+		}
+
+		workCtx = ctx
+
+		go func() {
+			close(started)
+			defer close(stopped)
+			<-ctx.Done()
+		}()
+
+		return nil
+	})
+
+	ctx := context.Background()
+
+	require.NoError(t, startStop.Start(ctx))
+	<-started
+	startStop.Stop()
+	require.ErrorIs(t, context.Cause(workCtx), ErrStop)
+}
+
+// A service with the more unusual case.
+type sampleServiceWithStopInit struct {
+	baseservice.BaseService
+	BaseStartStop
+
+	didStop bool
+
+	// Some simple state in the service which a started service taints. The
+	// purpose of this variable is to allow us to detect a data race allowed by
+	// BaseStartStop.
+	state bool
+}
+
+func (s *sampleServiceWithStopInit) Start(ctx context.Context) error {
+	ctx, shouldStart, stopped := s.StartInit(ctx)
+	if !shouldStart {
+		return nil
+	}
+
+	go func() {
+		defer close(stopped)
+		s.state = true
+		<-ctx.Done()
+	}()
+
+	return nil
+}
+
+func (s *sampleServiceWithStopInit) Stop() {
+	shouldStop, stopped, finalizeStop := s.StopInit()
+	if !shouldStop {
+		return
+	}
+
+	<-stopped
+	finalizeStop(s.didStop)
+}
+
+func TestWithStopInit(t *testing.T) {
+	t.Parallel()
+
+	testService(t, func(t *testing.T) serviceWithStopped { t.Helper(); return &sampleServiceWithStopInit{didStop: true} })
+
+	ctx := context.Background()
+
+	type testBundle struct{}
+
+	setup := func() (*sampleServiceWithStopInit, *testBundle) {
+		return &sampleServiceWithStopInit{}, &testBundle{}
+	}
+
+	t.Run("FinalizeDidStop", func(t *testing.T) {
+		t.Parallel()
+
+		service, _ := setup()
+		service.didStop = true // will set stopped
+
+		require.NoError(t, service.Start(ctx))
+
+		service.Stop()
+
+		require.False(t, service.started)
+		require.Nil(t, service.stopped)
+	})
+
+	t.Run("FinalizeDidNotStop", func(t *testing.T) {
+		t.Parallel()
+
+		service, _ := setup()
+		service.didStop = false // will NOT set stopped
+
+		require.NoError(t, service.Start(ctx))
+
+		service.Stop()
+
+		// service is still started because didStop was set to false
+		require.True(t, service.started)
+		require.NotNil(t, service.stopped)
+	})
 }
 
 func TestStopAllParallel(t *testing.T) {

commit 1384db07c6de0cf01f39a340d8b78b42ccd02f03

fix StopAndTest after ongoing Stop
This regression was introduced in #272, which I noticed because our demo
app is not currently cancelling active jobs gracefully before exiting.

This commit fixes the behavior by always cancelling the work context
upon `StopAndCancel()`, regardless of whether or not another shutdown is
in progress.

client.go
@@ -810,13 +810,13 @@ func (c *Client[TTx]) Stop(ctx context.Context) error {
 // no need to call this method if the context passed to Run is cancelled
 // instead.
 func (c *Client[TTx]) StopAndCancel(ctx context.Context) error {
+	c.baseService.Logger.InfoContext(ctx, c.baseService.Name+": Hard stop started; cancelling all work")
+	c.workCancel(rivercommon.ErrShutdown)
+
 	shouldStop, stopped, finalizeStop := c.baseStartStop.StopInit()
 	if !shouldStop {
 		return nil
 	}
-
-	c.baseService.Logger.InfoContext(ctx, c.baseService.Name+": Hard stop started; cancelling all work")
-	c.workCancel(rivercommon.ErrShutdown)
 
 	select {
 	case <-ctx.Done(): // stop context cancelled

client_test.go
@@ -645,39 +645,83 @@ func Test_Client(t *testing.T) {
 	t.Run("StopAndCancel", func(t *testing.T) {
 		t.Parallel()
 
-		client, _ := setup(t)
-		jobStartedChan := make(chan int64)
-		jobDoneChan := make(chan struct{})
-
-		type JobArgs struct {
-			JobArgsReflectKind[JobArgs]
+		type testBundle struct {
+			jobDoneChan    chan struct{}
+			jobStartedChan chan int64
 		}
 
-		AddWorker(client.config.Workers, WorkFunc(func(ctx context.Context, job *Job[JobArgs]) error {
-			jobStartedChan <- job.ID
-			<-ctx.Done()
-			require.ErrorIs(t, context.Cause(ctx), rivercommon.ErrShutdown)
-			close(jobDoneChan)
-			return nil
-		}))
+		setupStopAndCancel := func(t *testing.T) (*Client[pgx.Tx], *testBundle) {
+			t.Helper()
 
-		startClient(ctx, t, client)
+			client, _ := setup(t)
+			jobStartedChan := make(chan int64)
+			jobDoneChan := make(chan struct{})
 
-		insertRes, err := client.Insert(ctx, &JobArgs{}, nil)
-		require.NoError(t, err)
+			type JobArgs struct {
+				JobArgsReflectKind[JobArgs]
+			}
 
-		startedJobID := riverinternaltest.WaitOrTimeout(t, jobStartedChan)
-		require.Equal(t, insertRes.Job.ID, startedJobID)
+			AddWorker(client.config.Workers, WorkFunc(func(ctx context.Context, job *Job[JobArgs]) error {
+				jobStartedChan <- job.ID
+				<-ctx.Done()
+				require.ErrorIs(t, context.Cause(ctx), rivercommon.ErrShutdown)
+				close(jobDoneChan)
+				return nil
+			}))
 
-		select {
-		case <-client.Stopped():
-			t.Fatal("expected client to not be stopped yet")
-		default:
+			startClient(ctx, t, client)
+
+			insertRes, err := client.Insert(ctx, &JobArgs{}, nil)
+			require.NoError(t, err)
+
+			startedJobID := riverinternaltest.WaitOrTimeout(t, jobStartedChan)
+			require.Equal(t, insertRes.Job.ID, startedJobID)
+
+			select {
+			case <-client.Stopped():
+				t.Fatal("expected client to not be stopped yet")
+			default:
+			}
+
+			return client, &testBundle{
+				jobDoneChan:    jobDoneChan,
+				jobStartedChan: jobStartedChan,
+			}
 		}
 
-		require.NoError(t, client.StopAndCancel(ctx))
+		t.Run("OnItsOwn", func(t *testing.T) {
+			t.Parallel()
 
-		riverinternaltest.WaitOrTimeout(t, client.Stopped())
+			client, _ := setupStopAndCancel(t)
+
+			require.NoError(t, client.StopAndCancel(ctx))
+			riverinternaltest.WaitOrTimeout(t, client.Stopped())
+		})
+
+		t.Run("AfterStop", func(t *testing.T) {
+			t.Parallel()
+
+			client, bundle := setupStopAndCancel(t)
+
+			go func() {
+				require.NoError(t, client.Stop(ctx))
+			}()
+
+			select {
+			case <-client.Stopped():
+				t.Fatal("expected client to not be stopped yet")
+			case <-time.After(500 * time.Millisecond):
+			}
+
+			require.NoError(t, client.StopAndCancel(ctx))
+			riverinternaltest.WaitOrTimeout(t, client.Stopped())
+
+			select {
+			case <-bundle.jobDoneChan:
+			default:
+				t.Fatal("expected job to be have exited")
+			}
+		})
 	})
 }
 