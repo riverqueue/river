8f057ba3a0c571b7722162feff995cdd1651bbaa
Blake Gentry <blakesgentry@gmail.com>
2024-12-05 21:14:56 -0600

    working integration with lots of hacks and TODOs

--- rivershared/go.mod
+++ rivershared/go.mod
@@ -5,6 +5,7 @@ go 1.22.0
 toolchain go1.23.5
 
 require (
+	github.com/google/uuid v1.6.0
 	github.com/riverqueue/river v0.18.0
 	github.com/riverqueue/river/riverdriver v0.18.0
 	github.com/riverqueue/river/rivertype v0.18.0
@@ -15,7 +16,6 @@ require (
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/kr/text v0.2.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- rivershared/riverpilot/pilot.go
+++ rivershared/riverpilot/pilot.go
@@ -3,6 +3,7 @@ package riverpilot
 import (
 	"context"
 
+	"github.com/google/uuid"
 	"github.com/riverqueue/river/riverdriver"
 	"github.com/riverqueue/river/rivershared/baseservice"
 	"github.com/riverqueue/river/rivertype"
@@ -16,6 +17,13 @@ import (
 // This should be considered a River internal API and its stability is not
 // guaranteed. DO NOT USE.
 type Pilot interface {
+	JobGetAvailable(
+		ctx context.Context,
+		exec riverdriver.Executor,
+		state ProducerState,
+		params *riverdriver.JobGetAvailableParams,
+	) ([]*rivertype.JobRow, error)
+
 	JobInsertMany(
 		ctx context.Context,
 		tx riverdriver.ExecutorTx,
@@ -25,4 +33,16 @@ type Pilot interface {
 	JobSetStateIfRunningMany(ctx context.Context, tx riverdriver.ExecutorTx, params *riverdriver.JobSetStateIfRunningManyParams) ([]*rivertype.JobRow, error)
 
 	PilotInit(archetype *baseservice.Archetype)
+
+	ProducerKeepAlive(ctx context.Context, exec riverdriver.Executor, params *riverdriver.ProducerKeepAliveParams) error
+
+	// ProducerStateInit is called when a producer is started. It should return
+	// a new state object that will be used to track the producer's state.
+	ProducerStateInit(producerID uuid.UUID, queue string) ProducerState
+
+	ProducerShutdown(ctx context.Context, exec riverdriver.Executor, producerID uuid.UUID) error
+}
+
+type ProducerState interface {
+	JobFinish(job *rivertype.JobRow)
 }
