e30563db6d17469b1b284f9e28d110dd05ebf7ff
Blake Gentry <blakesgentry@gmail.com>
2025-02-04 21:33:08 -0600

    s/jobControlPayload/controlEventPayload

--- client.go
+++ client.go
@@ -1809,7 +1809,7 @@ func (c *Client[TTx]) notifyQueuePauseOrResume(ctx context.Context, tx riverdriv
 		slog.String("opts", fmt.Sprintf("%+v", opts)),
 	)
 
-	payload, err := json.Marshal(jobControlPayload{Action: action, Queue: queue})
+	payload, err := json.Marshal(controlEventPayload{Action: action, Queue: queue})
 	if err != nil {
 		return err
 	}
