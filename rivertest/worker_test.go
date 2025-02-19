8e66dd59b8f63b77606163a1f615cab8eedfed0b
Blake Gentry <bgentry@users.noreply.github.com>
2025-02-19 10:27:11 -0600

    rivertest.Worker fixes: handle nil config, rename Kind/EventKind, log panic trace (#775)
    * rivertest.NewWorker: handle nil config
    
    * rivertest.WorkResult: rename Kind to EventKind
    
    * rivertest.Worker: also log trace when handling panics
    
    * propagate testworker errors to caller
    
    If a panic occurs, return a `PanicError` struct containing the trace and
    panic cause. Do not use `tb.Fatalf` from within execution except for
    unexpected internal issues—instead propagate errors to the user.
    
    Also change the return type to `*WorkResult` since it may be optional
    when an internal error occurs.

--- rivertest/worker_test.go
+++ rivertest/worker_test.go
@@ -2,6 +2,7 @@ package rivertest
 
 import (
 	"context"
+	"errors"
 	"testing"
 	"time"
 
@@ -13,6 +14,8 @@ import (
 	"github.com/riverqueue/river/internal/riverinternaltest"
 	"github.com/riverqueue/river/riverdriver/riverpgxv5"
 	"github.com/riverqueue/river/rivershared/riversharedtest"
+	"github.com/riverqueue/river/rivershared/testfactory"
+	"github.com/riverqueue/river/rivershared/util/ptrutil"
 	"github.com/riverqueue/river/rivertype"
 )
 
@@ -21,6 +24,47 @@ type testArgs struct {
 }
 
 func (testArgs) Kind() string { return "rivertest_work_test" }
+
+func TestPanicError(t *testing.T) {
+	t.Parallel()
+
+	panicErr := &PanicError{Cause: errors.New("test panic error"), Trace: "test trace"}
+	require.Equal(t, "rivertest.PanicError: test panic error\ntest trace", panicErr.Error())
+}
+
+func TestWorker_NewWorker(t *testing.T) {
+	t.Parallel()
+
+	ctx := context.Background()
+
+	type testBundle struct {
+		config *river.Config
+		driver *riverpgxv5.Driver
+		tx     pgx.Tx
+	}
+
+	setup := func(t *testing.T) *testBundle {
+		t.Helper()
+
+		return &testBundle{
+			config: &river.Config{ID: "rivertest-worker"},
+			driver: riverpgxv5.New(nil),
+			tx:     riverinternaltest.TestTx(ctx, t),
+		}
+	}
+
+	t.Run("HandlesNilRiverConfig", func(t *testing.T) {
+		t.Parallel()
+
+		bundle := setup(t)
+
+		worker := river.WorkFunc(func(ctx context.Context, job *river.Job[testArgs]) error {
+			return nil
+		})
+		tw := NewWorker(t, bundle.driver, nil, worker)
+		require.NotNil(t, tw.config)
+	})
+}
 
 func TestWorker_Work(t *testing.T) {
 	t.Parallel()
@@ -83,7 +127,7 @@ func TestWorker_Work(t *testing.T) {
 		tw := NewWorker(t, bundle.driver, bundle.config, worker)
 		res, err := tw.Work(ctx, t, bundle.tx, testArgs{Value: "test"}, nil)
 		require.NoError(t, err)
-		require.Equal(t, river.EventKindJobCompleted, res.Kind)
+		require.Equal(t, river.EventKindJobCompleted, res.EventKind)
 	})
 
 	t.Run("Reusable", func(t *testing.T) {
@@ -97,10 +141,10 @@ func TestWorker_Work(t *testing.T) {
 		tw := NewWorker(t, bundle.driver, bundle.config, worker)
 		res, err := tw.Work(ctx, t, bundle.tx, testArgs{Value: "test"}, nil)
 		require.NoError(t, err)
-		require.Equal(t, river.EventKindJobCompleted, res.Kind)
+		require.Equal(t, river.EventKindJobCompleted, res.EventKind)
 		res, err = tw.Work(ctx, t, bundle.tx, testArgs{Value: "test2"}, nil)
 		require.NoError(t, err)
-		require.Equal(t, river.EventKindJobCompleted, res.Kind)
+		require.Equal(t, river.EventKindJobCompleted, res.EventKind)
 	})
 
 	t.Run("SetsCustomInsertOpts", func(t *testing.T) {
@@ -145,7 +189,7 @@ func TestWorker_Work(t *testing.T) {
 			Tags:        []string{"tag1", "tag2"},
 		})
 		require.NoError(t, err)
-		require.Equal(t, river.EventKindJobCompleted, res.Kind)
+		require.Equal(t, river.EventKindJobCompleted, res.EventKind)
 	})
 
 	t.Run("UniqueOptsAreIgnored", func(t *testing.T) {
@@ -171,7 +215,7 @@ func TestWorker_Work(t *testing.T) {
 			UniqueOpts: river.UniqueOpts{ByPeriod: 1 * time.Hour},
 		})
 		require.NoError(t, err)
-		require.Equal(t, river.EventKindJobCompleted, res.Kind)
+		require.Equal(t, river.EventKindJobCompleted, res.EventKind)
 	})
 
 	t.Run("ReturnsASnoozeEventKindWhenSnoozed", func(t *testing.T) {
@@ -186,7 +230,7 @@ func TestWorker_Work(t *testing.T) {
 
 		res, err := tw.Work(ctx, t, bundle.tx, testArgs{Value: "test"}, nil)
 		require.NoError(t, err)
-		require.Equal(t, river.EventKindJobSnoozed, res.Kind)
+		require.Equal(t, river.EventKindJobSnoozed, res.EventKind)
 	})
 
 	t.Run("ReturnsACancelEventKindWhenCancelled", func(t *testing.T) {
@@ -201,7 +245,7 @@ func TestWorker_Work(t *testing.T) {
 
 		res, err := tw.Work(ctx, t, bundle.tx, testArgs{Value: "test"}, nil)
 		require.NoError(t, err)
-		require.Equal(t, river.EventKindJobCancelled, res.Kind)
+		require.Equal(t, river.EventKindJobCancelled, res.EventKind)
 	})
 
 	t.Run("UsesACustomClockWhenProvided", func(t *testing.T) {
@@ -223,8 +267,64 @@ func TestWorker_Work(t *testing.T) {
 
 		res, err := tw.Work(ctx, t, bundle.tx, testArgs{Value: "test"}, nil)
 		require.NoError(t, err)
-		require.Equal(t, river.EventKindJobCompleted, res.Kind)
+		require.Equal(t, river.EventKindJobCompleted, res.EventKind)
 		require.WithinDuration(t, hourFromNow, *res.Job.FinalizedAt, time.Millisecond)
+	})
+
+	t.Run("ErrorFromWorker", func(t *testing.T) {
+		t.Parallel()
+
+		bundle := setup(t)
+
+		errToReturn := errors.New("test error")
+		worker := river.WorkFunc(func(ctx context.Context, job *river.Job[testArgs]) error {
+			return errToReturn
+		})
+		tw := NewWorker(t, bundle.driver, bundle.config, worker)
+
+		res, err := tw.Work(ctx, t, bundle.tx, testArgs{Value: "test"}, nil)
+		require.ErrorIs(t, err, errToReturn)
+		require.Equal(t, river.EventKindJobFailed, res.EventKind)
+	})
+
+	t.Run("PanicFromWorker", func(t *testing.T) {
+		t.Parallel()
+
+		bundle := setup(t)
+
+		errToReturn := errors.New("test panic error")
+		worker := river.WorkFunc(func(ctx context.Context, job *river.Job[testArgs]) error {
+			panic(errToReturn)
+		})
+		tw := NewWorker(t, bundle.driver, bundle.config, worker)
+
+		res, err := tw.Work(ctx, t, bundle.tx, testArgs{Value: "test"}, nil)
+		require.ErrorIs(t, err, &PanicError{})
+		require.ErrorContains(t, err, "test panic error")
+		require.Equal(t, river.EventKindJobFailed, res.EventKind)
+
+		var panicErr *PanicError
+		require.ErrorAs(t, err, &panicErr)
+		require.Equal(t, errToReturn, panicErr.Cause)
+		require.Contains(t, panicErr.Trace, "github.com/riverqueue/river/rivertest.TestWorker_Work")
+		require.Len(t, res.Job.Errors, 1)
+		require.Contains(t, res.Job.Errors[0].Error, "test panic error")
+	})
+
+	t.Run("ErrorsWithAlreadyClosedTransaction", func(t *testing.T) {
+		t.Parallel()
+
+		bundle := setup(t)
+
+		// Immediately roll back the transaction to force an error:
+		require.NoError(t, bundle.tx.Rollback(ctx))
+
+		worker := river.WorkFunc(func(ctx context.Context, job *river.Job[testArgs]) error { return nil })
+		tw := NewWorker(t, bundle.driver, bundle.config, worker)
+
+		res, err := tw.Work(ctx, t, bundle.tx, testArgs{Value: "test"}, nil)
+		require.ErrorContains(t, err, "failed to insert job: tx is closed")
+		require.Nil(t, res)
 	})
 }
 
@@ -284,7 +384,7 @@ func TestWorker_WorkJob(t *testing.T) {
 
 		res, err := testWorker.WorkJob(ctx, t, bundle.tx, insertRes.Job)
 		require.NoError(t, err)
-		require.Equal(t, river.EventKindJobCompleted, res.Kind)
+		require.Equal(t, river.EventKindJobCompleted, res.EventKind)
 	})
 
 	t.Run("JobCompleteTxWithInsertedJobRow", func(t *testing.T) {
@@ -309,10 +409,27 @@ func TestWorker_WorkJob(t *testing.T) {
 
 		res, err := testWorker.WorkJob(ctx, t, bundle.tx, insertRes.Job)
 		require.NoError(t, err)
-		require.Equal(t, river.EventKindJobCompleted, res.Kind)
+		require.Equal(t, river.EventKindJobCompleted, res.EventKind)
 
 		updatedJob, err := bundle.driver.UnwrapExecutor(bundle.tx).JobGetByID(ctx, insertRes.Job.ID)
 		require.NoError(t, err)
 		require.Equal(t, rivertype.JobStateCompleted, updatedJob.State)
+	})
+
+	t.Run("ErrorsWhenGivenAlreadyCompletedJob", func(t *testing.T) {
+		t.Parallel()
+
+		ctx := context.Background()
+		testWorker, bundle := setup(t)
+
+		job := testfactory.Job(ctx, t, bundle.driver.UnwrapExecutor(bundle.tx), &testfactory.JobOpts{
+			EncodedArgs: []byte(`{"value": "test"}`),
+			Kind:        ptrutil.Ptr("rivertest_work_test"),
+			State:       ptrutil.Ptr(rivertype.JobStateCompleted),
+		})
+
+		res, err := testWorker.WorkJob(ctx, t, bundle.tx, job)
+		require.ErrorContains(t, err, "failed to update job to running state")
+		require.Nil(t, res)
 	})
 }
