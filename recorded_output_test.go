057a8dd8c5da9f276a35554c998ec45d1e1307f6
Blake Gentry <blakesgentry@gmail.com>
2025-02-20 16:37:46 -0600

    limit recorded output to 32MB in size

--- recorded_output_test.go
+++ recorded_output_test.go
@@ -3,6 +3,7 @@ package river
 import (
 	"context"
 	"encoding/json"
+	"strings"
 	"testing"
 
 	"github.com/jackc/pgx/v5"
@@ -44,14 +45,14 @@ func Test_RecordedOutput(t *testing.T) {
 
 		client, _ := setup(t)
 
-		subChan := subscribe(t, client)
-		startClient(ctx, t, client)
-
 		validOutput := myOutput{Message: "it worked"}
 		expectedOutput := `{"output":{"message":"it worked"}}`
 		AddWorker(client.config.Workers, WorkFunc(func(ctx context.Context, job *Job[JobArgs]) error {
 			return RecordOutput(ctx, validOutput)
 		}))
+
+		subChan := subscribe(t, client)
+		startClient(ctx, t, client)
 
 		insertRes, err := client.Insert(ctx, JobArgs{}, nil)
 		require.NoError(t, err)
@@ -70,15 +71,14 @@ func Test_RecordedOutput(t *testing.T) {
 
 		client, _ := setup(t)
 
-		// Subscribe to job failure events
-		subChan := subscribe(t, client)
-		startClient(ctx, t, client)
-
 		// Use an invalid output value (a channel, which cannot be marshaled to JSON)
 		var invalidOutput chan int
 		AddWorker(client.config.Workers, WorkFunc(func(ctx context.Context, job *Job[JobArgs]) error {
 			return RecordOutput(ctx, invalidOutput)
 		}))
+
+		subChan := subscribe(t, client)
+		startClient(ctx, t, client)
 
 		insertRes, err := client.Insert(ctx, JobArgs{}, nil)
 		require.NoError(t, err)
@@ -102,13 +102,13 @@ func Test_RecordedOutput(t *testing.T) {
 
 		client, _ := setup(t)
 
-		subChan := subscribe(t, client)
-		startClient(ctx, t, client)
-
 		newOutput := myOutput{Message: "new output"}
 		AddWorker(client.config.Workers, WorkFunc(func(ctx context.Context, job *Job[JobArgs]) error {
 			return RecordOutput(ctx, newOutput)
 		}))
+
+		subChan := subscribe(t, client)
+		startClient(ctx, t, client)
 
 		// Insert a job with pre-existing metadata (including an output key)
 		initialMeta := `{"existing":"value","output":"old"}`
@@ -124,5 +124,36 @@ func Test_RecordedOutput(t *testing.T) {
 		jobFromDB, err := client.JobGet(ctx, insertRes.Job.ID)
 		require.NoError(t, err)
 		require.JSONEq(t, expectedMeta, string(jobFromDB.Metadata))
+	})
+
+	t.Run("OutputTooLarge", func(t *testing.T) {
+		t.Parallel()
+
+		client, _ := setup(t)
+
+		AddWorker(client.config.Workers, WorkFunc(func(ctx context.Context, job *Job[JobArgs]) error {
+			// Record an output of 32MB + 1 byte:
+			err := RecordOutput(ctx, strings.Repeat("x", 32*1024*1024+1))
+			require.ErrorContains(t, err, "output is too large")
+			return err
+		}))
+
+		subChan := subscribe(t, client)
+		startClient(ctx, t, client)
+
+		insertRes, err := client.Insert(ctx, JobArgs{}, nil)
+		require.NoError(t, err)
+
+		event := riversharedtest.WaitOrTimeout(t, subChan)
+		require.Equal(t, EventKindJobFailed, event.Kind)
+		require.NotEmpty(t, event.Job.Errors)
+		require.Contains(t, event.Job.Errors[0].Error, "output is too large")
+
+		jobFromDB, err := client.JobGet(ctx, insertRes.Job.ID)
+		require.NoError(t, err)
+		var meta map[string]any
+		require.NoError(t, json.Unmarshal(jobFromDB.Metadata, &meta))
+		_, ok := meta["output"]
+		require.False(t, ok, "output key should not be set in metadata")
 	})
 }
