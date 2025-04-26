riverqueue/river

commit 85cc50b20fe05f660d79e4e47e647ec18852ce21

Full schema insertion + schema testing (#848)
* Full schema insertion + schema testing

Here, introduce a full version of schema injection as initially started
in #798, and exposing a new `Schema` option to make it configurable even
outside the test suite.

This involved fixing a lot of bugs from #798, and the only way to make
it possible to root them all out was to make full use of schemas across
the entire test suite. The original "test DB manager" system has been
replaced with a new `riverschematest.TestSchema` helper that generates a
schema for use with a test case or prefers an existing idle one that was
already generated for the same test run.

`TestSchema` runs migrations for generated schemas which also means we
don't need to use `testdbman` anymore, with tests capable of
bootstrapping themselves at run time. We reuse schemas to avoid this
extra work when possible, but migrating a new schema is also
surprisingly fast, taking up to 50ms, but getting more down to a stable
~10ms once things are warmed up. Here's a series of sample timings:

    $ go test ./rivermigrate -run TestMigrator/MigrateUpDefault -test.v -count 50 | grep 'migrations ran in'
        river_migrate_test.go:492: migrations ran in 45.571209ms
        river_migrate_test.go:492: migrations ran in 24.642458ms
        river_migrate_test.go:492: migrations ran in 16.749708ms
        river_migrate_test.go:492: migrations ran in 22.970375ms
        river_migrate_test.go:492: migrations ran in 16.201375ms
        river_migrate_test.go:492: migrations ran in 15.727625ms
        river_migrate_test.go:492: migrations ran in 13.291333ms
        river_migrate_test.go:492: migrations ran in 13.680708ms
        river_migrate_test.go:492: migrations ran in 14.867416ms
        river_migrate_test.go:492: migrations ran in 15.631916ms
        river_migrate_test.go:492: migrations ran in 13.873791ms
        river_migrate_test.go:492: migrations ran in 14.8645ms

rivershared/riversharedtest/riversharedtest.go
@@ -18,6 +18,7 @@ import (
 
 	"github.com/riverqueue/river/rivershared/baseservice"
 	"github.com/riverqueue/river/rivershared/slogtest"
+	"github.com/riverqueue/river/rivershared/util/testutil"
 )
 
 // BaseServiceArchetype returns a new base service suitable for use in tests.
@@ -46,14 +47,52 @@ func DBPool(ctx context.Context, tb testing.TB) *pgxpool.Pool {
 	tb.Helper()
 
 	dbPoolOnce.Do(func() {
-		var err error
-		dbPool, err = pgxpool.New(ctx, cmp.Or(
-			os.Getenv("TEST_DATABASE_URL"),
-			"postgres://localhost:5432/river_test",
-		))
+		config, err := pgxpool.ParseConfig(TestDatabaseURL())
+		require.NoError(tb, err)
+
+		config.AfterConnect = func(ctx context.Context, conn *pgx.Conn) error {
+			// Empty the search path so that tests using riverdbtest are
+			// forced to pass a schema to clients and any other database
+			// operations they invoke. Calls do not accidentally fall back to a
+			// default schema, which would potentially hide bugs where we
+			// weren't properly referencing a schema explicitly.
+			_, err := conn.Exec(ctx, "SET search_path TO ''")
+
+			// This should not be a `require` because the callback may run long
+			// after the original test has completed.
+			if err != nil && !errors.Is(err, context.Canceled) {
+				panic(err)
+			}
+
+			return nil
+		}
+
+		dbPool, err = pgxpool.NewWithConfig(ctx, config)
 		require.NoError(tb, err)
 	})
 	require.NotNil(tb, dbPool) // die in case initial connect from another test failed
+
+	return dbPool
+}
+
+// DBPoolClone returns a disposable clone of DBPool. Share resources by using
+// DBPool when possible, but this is useless for areas like stress tests where
+// context cancellations are likely to end up closing the pool.
+//
+// Unlike DBPool, adds a test cleanup hook that closes the pool after run.
+func DBPoolClone(ctx context.Context, tb testing.TB) *pgxpool.Pool {
+	tb.Helper()
+
+	dbPool := DBPool(ctx, tb)
+
+	config := dbPool.Config()
+	config.MaxConns = 4 // dramatically reduce max allowed conns for clones so we they don't clobber the database server
+
+	var err error
+	dbPool, err = pgxpool.NewWithConfig(ctx, config)
+	require.NoError(tb, err)
+
+	tb.Cleanup(dbPool.Close)
 
 	return dbPool
 }
@@ -74,72 +113,32 @@ func Logger(tb testing.TB) *slog.Logger {
 
 // Logger returns a logger suitable for use in tests which outputs only at warn
 // or above. Useful in tests where particularly noisy log output is expected.
-func LoggerWarn(tb testing.TB) *slog.Logger {
+func LoggerWarn(tb testutil.TestingTB) *slog.Logger {
 	tb.Helper()
 	return slogtest.NewLogger(tb, &slog.HandlerOptions{Level: slog.LevelWarn})
 }
 
-// TestTx starts a test transaction that's rolled back automatically as the test
-// case is cleaning itself up.
-//
-// This variant uses the default database pool from DBPool that points to
-// `TEST_DATABASE_URL` or `river_test` if the former wasn't specified.
-func TestTx(ctx context.Context, tb testing.TB) pgx.Tx {
-	tb.Helper()
-	return TestTxPool(ctx, tb, DBPool(ctx, tb))
-}
+// TestDatabaseURL returns `TEST_DATABASE_URL` or a default URL pointing to
+// `river_test` and with suitable connection configuration defaults.
+func TestDatabaseURL() string {
+	return cmp.Or(
+		os.Getenv("TEST_DATABASE_URL"),
 
-// TestTxPool starts a test transaction that's rolled back automatically as the
-// test case is cleaning itself up.
-//
-// This variant starts the test transaction on the specified database pool.
-func TestTxPool(ctx context.Context, tb testing.TB, dbPool *pgxpool.Pool) pgx.Tx {
-	tb.Helper()
-
-	tx, err := dbPool.Begin(ctx)
-	require.NoError(tb, err)
-
-	tb.Cleanup(func() {
-		// Tests may inerit context from `t.Context()` which is cancelled after
-		// tests run and before calling clean up. We need a non-cancelled
-		// context to issue rollback here, so use a bit of a bludgeon to do so
-		// with `context.WithoutCancel()`.
-		ctx := context.WithoutCancel(ctx)
-
-		err := tx.Rollback(ctx)
-
-		if err == nil {
-			return
-		}
-
-		// Try to look for an error on rollback because it does occasionally
-		// reveal a real problem in the way a test is written. However, allow
-		// tests to roll back their transaction early if they like, so ignore
-		// `ErrTxClosed`.
-		if errors.Is(err, pgx.ErrTxClosed) {
-			return
-		}
-
-		// In case of a cancelled context during a database operation, which
-		// happens in many tests, pgx seems to not only roll back the
-		// transaction, but closes the connection, and returns this error on
-		// rollback. Allow this error since it's hard to prevent it in our flows
-		// that use contexts heavily.
-		if err.Error() == "conn closed" {
-			return
-		}
-
-		// Similar to the above, but a newly appeared error that wraps the
-		// above. As far as I can tell, no error variables are available to use
-		// with `errors.Is`.
-		if err.Error() == "failed to deallocate cached statement(s): conn closed" {
-			return
-		}
-
-		require.NoError(tb, err)
-	})
-
-	return tx
+		// 100 conns is the default maximum for Homebrew.
+		//
+		// It'd be nice to be able to set this number really high because it'd
+		// mean less waiting time acquiring connections in tests, but with
+		// default settings, contention between tests/test packages leading to
+		// exhausion on the Postgres server is definitely a problem. At numbers
+		// >75 I started seeing a lot of errors between tests within a single
+		// package, and worse yet, at numbers >=20 I saw major problems between
+		// packages (i.e. as parallel packages run at the same time).
+		//
+		// 15 is about as high as I found I could set it while keeping test runs
+		// stable. This could be much higher in areas where we know Postgres is
+		// configured with more allowed max connections.
+		"postgres://localhost:5432/river_test?pool_max_conns=15&sslmode=disable",
+	)
 }
 
 // TimeStub implements baseservice.TimeGeneratorWithStub to allow time to be

example_batch_insert_test.go
@@ -8,9 +8,11 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 
 	"github.com/riverqueue/river"
-	"github.com/riverqueue/river/internal/riverinternaltest"
+	"github.com/riverqueue/river/riverdbtest"
 	"github.com/riverqueue/river/riverdriver/riverpgxv5"
+	"github.com/riverqueue/river/rivershared/riversharedtest"
 	"github.com/riverqueue/river/rivershared/util/slogutil"
+	"github.com/riverqueue/river/rivershared/util/testutil"
 )
 
 type BatchInsertArgs struct{}
@@ -33,16 +35,11 @@ func (w *BatchInsertWorker) Work(ctx context.Context, job *river.Job[BatchInsert
 func Example_batchInsert() {
 	ctx := context.Background()
 
-	dbPool, err := pgxpool.NewWithConfig(ctx, riverinternaltest.DatabaseConfig("river_test_example"))
+	dbPool, err := pgxpool.New(ctx, riversharedtest.TestDatabaseURL())
 	if err != nil {
 		panic(err)
 	}
 	defer dbPool.Close()
-
-	// Required for the purpose of this test, but not necessary in real usage.
-	if err := riverinternaltest.TruncateRiverTables(ctx, dbPool); err != nil {
-		panic(err)
-	}
 
 	workers := river.NewWorkers()
 	river.AddWorker(workers, &BatchInsertWorker{})
@@ -52,7 +49,8 @@ func Example_batchInsert() {
 		Queues: map[string]river.QueueConfig{
 			river.QueueDefault: {MaxWorkers: 100},
 		},
-		TestOnly: true, // suitable only for use in tests; remove for live environments
+		Schema:   riverdbtest.TestSchema(ctx, testutil.PanicTB(), riverpgxv5.New(dbPool), nil), // only necessary for the example test
+		TestOnly: true,                                                                         // suitable only for use in tests; remove for live environments
 		Workers:  workers,
 	})
 	if err != nil {

example_client_from_context_database_sql_test.go
@@ -11,9 +11,12 @@ import (
 	_ "github.com/jackc/pgx/v5/stdlib"
 
 	"github.com/riverqueue/river"
-	"github.com/riverqueue/river/internal/riverinternaltest"
+	"github.com/riverqueue/river/riverdbtest"
 	"github.com/riverqueue/river/riverdriver/riverdatabasesql"
+	"github.com/riverqueue/river/rivershared/riversharedtest"
 	"github.com/riverqueue/river/rivershared/util/slogutil"
+	"github.com/riverqueue/river/rivershared/util/testutil"
+	"github.com/riverqueue/river/rivershared/util/urlutil"
 )
 
 type ContextClientSQLArgs struct{}
@@ -41,8 +44,7 @@ func (w *ContextClientSQLWorker) Work(ctx context.Context, job *river.Job[Contex
 func ExampleClientFromContext_databaseSQL() {
 	ctx := context.Background()
 
-	config := riverinternaltest.DatabaseConfig("river_test_example")
-	db, err := sql.Open("pgx", config.ConnString())
+	db, err := sql.Open("pgx", urlutil.DatabaseSQLCompatibleURL(riversharedtest.TestDatabaseURL()))
 	if err != nil {
 		panic(err)
 	}
@@ -59,7 +61,8 @@ func ExampleClientFromContext_databaseSQL() {
 		},
 		FetchCooldown:     10 * time.Millisecond,
 		FetchPollInterval: 10 * time.Millisecond,
-		TestOnly:          true, // suitable only for use in tests; remove for live environments
+		Schema:            riverdbtest.TestSchema(ctx, testutil.PanicTB(), riverdatabasesql.New(db), nil), // only necessary for the example test
+		TestOnly:          true,                                                                           // suitable only for use in tests; remove for live environments
 		Workers:           workers,
 	})
 	if err != nil {

example_client_from_context_test.go
@@ -10,9 +10,11 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 
 	"github.com/riverqueue/river"
-	"github.com/riverqueue/river/internal/riverinternaltest"
+	"github.com/riverqueue/river/riverdbtest"
 	"github.com/riverqueue/river/riverdriver/riverpgxv5"
+	"github.com/riverqueue/river/rivershared/riversharedtest"
 	"github.com/riverqueue/river/rivershared/util/slogutil"
+	"github.com/riverqueue/river/rivershared/util/testutil"
 )
 
 type ContextClientArgs struct{}
@@ -40,16 +42,11 @@ func (w *ContextClientWorker) Work(ctx context.Context, job *river.Job[ContextCl
 func ExampleClientFromContext_pgx() {
 	ctx := context.Background()
 
-	dbPool, err := pgxpool.NewWithConfig(ctx, riverinternaltest.DatabaseConfig("river_test_example"))
+	dbPool, err := pgxpool.New(ctx, riversharedtest.TestDatabaseURL())
 	if err != nil {
 		panic(err)
 	}
 	defer dbPool.Close()
-
-	// Required for the purpose of this test, but not necessary in real usage.
-	if err := riverinternaltest.TruncateRiverTables(ctx, dbPool); err != nil {
-		panic(err)
-	}
 
 	workers := river.NewWorkers()
 	river.AddWorker(workers, &ContextClientWorker{})
@@ -60,7 +57,8 @@ func ExampleClientFromContext_pgx() {
 		Queues: map[string]river.QueueConfig{
 			river.QueueDefault: {MaxWorkers: 10},
 		},
-		TestOnly: true, // suitable only for use in tests; remove for live environments
+		Schema:   riverdbtest.TestSchema(ctx, testutil.PanicTB(), riverpgxv5.New(dbPool), nil), // only necessary for the example test
+		TestOnly: true,                                                                         // suitable only for use in tests; remove for live environments
 		Workers:  workers,
 	})
 	if err != nil {

example_complete_job_within_tx_test.go
@@ -8,9 +8,11 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 
 	"github.com/riverqueue/river"
-	"github.com/riverqueue/river/internal/riverinternaltest"
+	"github.com/riverqueue/river/riverdbtest"
 	"github.com/riverqueue/river/riverdriver/riverpgxv5"
+	"github.com/riverqueue/river/rivershared/riversharedtest"
 	"github.com/riverqueue/river/rivershared/util/slogutil"
+	"github.com/riverqueue/river/rivershared/util/testutil"
 )
 
 type TransactionalArgs struct{}
@@ -58,27 +60,22 @@ func (w *TransactionalWorker) Work(ctx context.Context, job *river.Job[Transacti
 func Example_completeJobWithinTx() {
 	ctx := context.Background()
 
-	dbPool, err := pgxpool.NewWithConfig(ctx, riverinternaltest.DatabaseConfig("river_test_example"))
+	dbPool, err := pgxpool.New(ctx, riversharedtest.TestDatabaseURL())
 	if err != nil {
 		panic(err)
 	}
 	defer dbPool.Close()
 
-	// Required for the purpose of this test, but not necessary in real usage.
-	if err := riverinternaltest.TruncateRiverTables(ctx, dbPool); err != nil {
-		panic(err)
-	}
-
 	workers := river.NewWorkers()
 	river.AddWorker(workers, &TransactionalWorker{dbPool: dbPool})
-	river.AddWorker(workers, &SortWorker{})
 
 	riverClient, err := river.NewClient(riverpgxv5.New(dbPool), &river.Config{
 		Logger: slog.New(&slogutil.SlogMessageOnlyHandler{Level: slog.LevelWarn}),
 		Queues: map[string]river.QueueConfig{
 			river.QueueDefault: {MaxWorkers: 100},
 		},
-		TestOnly: true, // suitable only for use in tests; remove for live environments
+		Schema:   riverdbtest.TestSchema(ctx, testutil.PanicTB(), riverpgxv5.New(dbPool), nil), // only necessary for the example test
+		TestOnly: true,                                                                         // suitable only for use in tests; remove for live environments
 		Workers:  workers,
 	})
 	if err != nil {

example_cron_job_test.go
@@ -9,9 +9,11 @@ import (
 	"github.com/robfig/cron/v3"
 
 	"github.com/riverqueue/river"
-	"github.com/riverqueue/river/internal/riverinternaltest"
+	"github.com/riverqueue/river/riverdbtest"
 	"github.com/riverqueue/river/riverdriver/riverpgxv5"
+	"github.com/riverqueue/river/rivershared/riversharedtest"
 	"github.com/riverqueue/river/rivershared/util/slogutil"
+	"github.com/riverqueue/river/rivershared/util/testutil"
 )
 
 type CronJobArgs struct{}
@@ -35,16 +37,11 @@ func (w *CronJobWorker) Work(ctx context.Context, job *river.Job[CronJobArgs]) e
 func Example_cronJob() {
 	ctx := context.Background()
 
-	dbPool, err := pgxpool.NewWithConfig(ctx, riverinternaltest.DatabaseConfig("river_test_example"))
+	dbPool, err := pgxpool.New(ctx, riversharedtest.TestDatabaseURL())
 	if err != nil {
 		panic(err)
 	}
 	defer dbPool.Close()
-
-	// Required for the purpose of this test, but not necessary in real usage.
-	if err := riverinternaltest.TruncateRiverTables(ctx, dbPool); err != nil {
-		panic(err)
-	}
 
 	workers := river.NewWorkers()
 	river.AddWorker(workers, &CronJobWorker{})
@@ -68,7 +65,8 @@ func Example_cronJob() {
 		Queues: map[string]river.QueueConfig{
 			river.QueueDefault: {MaxWorkers: 100},
 		},
-		TestOnly: true, // suitable only for use in tests; remove for live environments
+		Schema:   riverdbtest.TestSchema(ctx, testutil.PanicTB(), riverpgxv5.New(dbPool), nil), // only necessary for the example test
+		TestOnly: true,                                                                         // suitable only for use in tests; remove for live environments
 		Workers:  workers,
 	})
 	if err != nil {

example_custom_insert_opts_test.go
@@ -8,9 +8,11 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 
 	"github.com/riverqueue/river"
-	"github.com/riverqueue/river/internal/riverinternaltest"
+	"github.com/riverqueue/river/riverdbtest"
 	"github.com/riverqueue/river/riverdriver/riverpgxv5"
+	"github.com/riverqueue/river/rivershared/riversharedtest"
 	"github.com/riverqueue/river/rivershared/util/slogutil"
+	"github.com/riverqueue/river/rivershared/util/testutil"
 )
 
 type AlwaysHighPriorityArgs struct{}
@@ -56,16 +58,11 @@ func (w *SometimesHighPriorityWorker) Work(ctx context.Context, job *river.Job[S
 func Example_customInsertOpts() {
 	ctx := context.Background()
 
-	dbPool, err := pgxpool.NewWithConfig(ctx, riverinternaltest.DatabaseConfig("river_test_example"))
+	dbPool, err := pgxpool.New(ctx, riversharedtest.TestDatabaseURL())
 	if err != nil {
 		panic(err)
 	}
 	defer dbPool.Close()
-
-	// Required for the purpose of this test, but not necessary in real usage.
-	if err := riverinternaltest.TruncateRiverTables(ctx, dbPool); err != nil {
-		panic(err)
-	}
 
 	workers := river.NewWorkers()
 	river.AddWorker(workers, &AlwaysHighPriorityWorker{})
@@ -77,7 +74,8 @@ func Example_customInsertOpts() {
 			river.QueueDefault: {MaxWorkers: 100},
 			"high_priority":    {MaxWorkers: 100},
 		},
-		TestOnly: true, // suitable only for use in tests; remove for live environments
+		Schema:   riverdbtest.TestSchema(ctx, testutil.PanicTB(), riverpgxv5.New(dbPool), nil), // only necessary for the example test
+		TestOnly: true,                                                                         // suitable only for use in tests; remove for live environments
 		Workers:  workers,
 	})
 	if err != nil {

commit 89a6a7749d7ee82c1f63826d9e59e14c7e147be7

Use normal test helpers in example tests with `testutil.PanicTB`
Uses `testutil.PanicTB` introduced in #848 to switch to using standard
`riversharedtest` helpers like `WaitOrTimeoutN` instead of the ones
created for example tests specifically like `waitForNJobs`. Cuts down on
the number of helpers required and lets us use example tests across
packages without having to copy and paste the same `waitForNJobs` into
each one.

rivershared/riversharedtest/riversharedtest.go
@@ -199,7 +199,7 @@ func WaitOrTimeout[T any](tb testing.TB, waitChan <-chan T) T {
 // through, and returns it if they do, but times out after a reasonable amount
 // of time.  Useful to guarantee that test cases don't hang forever, even in the
 // event of something wrong.
-func WaitOrTimeoutN[T any](tb testing.TB, waitChan <-chan T, numValues int) []T {
+func WaitOrTimeoutN[T any](tb testutil.TestingTB, waitChan <-chan T, numValues int) []T {
 	tb.Helper()
 
 	var (

common_test.go
@@ -3,12 +3,8 @@ package river_test
 import (
 	"context"
 	"fmt"
-	"time"
 
 	"github.com/riverqueue/river"
-	"github.com/riverqueue/river/rivershared/riversharedtest"
-	"github.com/riverqueue/river/rivershared/util/sliceutil"
-	"github.com/riverqueue/river/rivertype"
 )
 
 //
@@ -27,29 +23,4 @@ type NoOpWorker struct {
 func (w *NoOpWorker) Work(ctx context.Context, job *river.Job[NoOpArgs]) error {
 	fmt.Printf("NoOpWorker.Work ran\n")
 	return nil
-}
-
-// Wait on the given subscription channel for numJobs. Times out with a panic if
-// jobs take too long to be received.
-func waitForNJobs(subscribeChan <-chan *river.Event, numJobs int) []*rivertype.JobRow { //nolint:unparam
-	var (
-		timeout  = riversharedtest.WaitTimeout()
-		deadline = time.Now().Add(timeout)
-		events   = make([]*river.Event, 0, numJobs)
-	)
-
-	for {
-		select {
-		case event := <-subscribeChan:
-			events = append(events, event)
-
-			if len(events) >= numJobs {
-				return sliceutil.Map(events, func(e *river.Event) *rivertype.JobRow { return e.Job })
-			}
-
-		case <-time.After(time.Until(deadline)):
-			panic(fmt.Sprintf("waitForNJobs timed out after waiting %s (received %d job(s), wanted %d)",
-				timeout, len(events), numJobs))
-		}
-	}
 }

example_batch_insert_test.go
@@ -77,7 +77,8 @@ func Example_batchInsert() {
 	}
 	fmt.Printf("Inserted %d jobs\n", len(results))
 
-	waitForNJobs(subscribeChan, 5)
+	// Wait for jobs to complete. Only needed for purposes of the example test.
+	riversharedtest.WaitOrTimeoutN(testutil.PanicTB(), subscribeChan, 5)
 
 	if err := riverClient.Stop(ctx); err != nil {
 		panic(err)

example_client_from_context_database_sql_test.go
@@ -80,7 +80,8 @@ func ExampleClientFromContext_databaseSQL() {
 		panic(err)
 	}
 
-	waitForNJobs(subscribeChan, 1)
+	// Wait for jobs to complete. Only needed for purposes of the example test.
+	riversharedtest.WaitOrTimeoutN(testutil.PanicTB(), subscribeChan, 1)
 
 	if err := riverClient.Stop(ctx); err != nil {
 		panic(err)

example_client_from_context_test.go
@@ -76,7 +76,8 @@ func ExampleClientFromContext_pgx() {
 		panic(err)
 	}
 
-	waitForNJobs(subscribeChan, 1)
+	// Wait for jobs to complete. Only needed for purposes of the example test.
+	riversharedtest.WaitOrTimeoutN(testutil.PanicTB(), subscribeChan, 1)
 
 	if err := riverClient.Stop(ctx); err != nil {
 		panic(err)

example_complete_job_within_tx_test.go
@@ -94,7 +94,8 @@ func Example_completeJobWithinTx() {
 		panic(err)
 	}
 
-	waitForNJobs(subscribeChan, 1)
+	// Wait for jobs to complete. Only needed for purposes of the example test.
+	riversharedtest.WaitOrTimeoutN(testutil.PanicTB(), subscribeChan, 1)
 
 	if err := riverClient.Stop(ctx); err != nil {
 		panic(err)

example_cron_job_test.go
@@ -83,7 +83,8 @@ func Example_cronJob() {
 		panic(err)
 	}
 
-	waitForNJobs(subscribeChan, 1)
+	// Wait for jobs to complete. Only needed for purposes of the example test.
+	riversharedtest.WaitOrTimeoutN(testutil.PanicTB(), subscribeChan, 1)
 
 	if err := riverClient.Stop(ctx); err != nil {
 		panic(err)

example_custom_insert_opts_test.go
@@ -106,7 +106,8 @@ func Example_customInsertOpts() {
 		panic(err)
 	}
 
-	waitForNJobs(subscribeChan, 2)
+	// Wait for jobs to complete. Only needed for purposes of the example test.
+	riversharedtest.WaitOrTimeoutN(testutil.PanicTB(), subscribeChan, 2)
 
 	if err := riverClient.Stop(ctx); err != nil {
 		panic(err)

example_error_handler_test.go
@@ -100,13 +100,15 @@ func Example_errorHandler() {
 
 	// Wait for the first job before inserting another to guarantee test output
 	// is ordered correctly.
-	waitForNJobs(subscribeChan, 1)
+	// Wait for jobs to complete. Only needed for purposes of the example test.
+	riversharedtest.WaitOrTimeoutN(testutil.PanicTB(), subscribeChan, 1)
 
 	if _, err = riverClient.Insert(ctx, ErroringArgs{ShouldPanic: true}, nil); err != nil {
 		panic(err)
 	}
 
-	waitForNJobs(subscribeChan, 1)
+	// Wait for jobs to complete. Only needed for purposes of the example test.
+	riversharedtest.WaitOrTimeoutN(testutil.PanicTB(), subscribeChan, 1)
 
 	if err := riverClient.Stop(ctx); err != nil {
 		panic(err)

example_global_hooks_test.go
@@ -98,7 +98,8 @@ func Example_globalHooks() {
 		panic(err)
 	}
 
-	waitForNJobs(subscribeChan, 1)
+	// Wait for jobs to complete. Only needed for purposes of the example test.
+	riversharedtest.WaitOrTimeoutN(testutil.PanicTB(), subscribeChan, 1)
 
 	if err := riverClient.Stop(ctx); err != nil {
 		panic(err)

example_global_middleware_test.go
@@ -98,7 +98,8 @@ func Example_globalMiddleware() {
 		panic(err)
 	}
 
-	waitForNJobs(subscribeChan, 1)
+	// Wait for jobs to complete. Only needed for purposes of the example test.
+	riversharedtest.WaitOrTimeoutN(testutil.PanicTB(), subscribeChan, 1)
 
 	if err := riverClient.Stop(ctx); err != nil {
 		panic(err)

example_insert_and_work_test.go
@@ -92,7 +92,8 @@ func Example_insertAndWork() {
 		panic(err)
 	}
 
-	waitForNJobs(subscribeChan, 1)
+	// Wait for jobs to complete. Only needed for purposes of the example test.
+	riversharedtest.WaitOrTimeoutN(testutil.PanicTB(), subscribeChan, 1)
 
 	if err := riverClient.Stop(ctx); err != nil {
 		panic(err)

example_job_args_hooks_test.go
@@ -116,7 +116,8 @@ func Example_jobArgsHooks() {
 		panic(err)
 	}
 
-	waitForNJobs(subscribeChan, 1)
+	// Wait for jobs to complete. Only needed for purposes of the example test.
+	riversharedtest.WaitOrTimeoutN(testutil.PanicTB(), subscribeChan, 1)
 
 	if err := riverClient.Stop(ctx); err != nil {
 		panic(err)

example_job_cancel_from_client_test.go
@@ -90,7 +90,8 @@ func Example_jobCancelFromClient() {
 	if _, err = riverClient.JobCancel(ctx, insertRes.Job.ID); err != nil {
 		panic(err)
 	}
-	waitForNJobs(subscribeChan, 1)
+	// Wait for jobs to complete. Only needed for purposes of the example test.
+	riversharedtest.WaitOrTimeoutN(testutil.PanicTB(), subscribeChan, 1)
 
 	if err := riverClient.Stop(ctx); err != nil {
 		panic(err)

example_job_cancel_test.go
@@ -71,7 +71,8 @@ func Example_jobCancel() { //nolint:dupl
 	if _, err = riverClient.Insert(ctx, CancellingArgs{ShouldCancel: true}, nil); err != nil {
 		panic(err)
 	}
-	waitForNJobs(subscribeChan, 1)
+	// Wait for jobs to complete. Only needed for purposes of the example test.
+	riversharedtest.WaitOrTimeoutN(testutil.PanicTB(), subscribeChan, 1)
 
 	if err := riverClient.Stop(ctx); err != nil {
 		panic(err)

example_job_snooze_test.go
@@ -74,7 +74,8 @@ func Example_jobSnooze() { //nolint:dupl
 	if _, err = riverClient.Insert(ctx, SnoozingArgs{ShouldSnooze: true}, nil); err != nil {
 		panic(err)
 	}
-	waitForNJobs(subscribeChan, 1)
+	// Wait for jobs to complete. Only needed for purposes of the example test.
+	riversharedtest.WaitOrTimeoutN(testutil.PanicTB(), subscribeChan, 1)
 
 	if err := riverClient.Stop(ctx); err != nil {
 		panic(err)

example_periodic_job_test.go
@@ -76,7 +76,8 @@ func Example_periodicJob() {
 		panic(err)
 	}
 
-	waitForNJobs(subscribeChan, 1)
+	// Wait for jobs to complete. Only needed for purposes of the example test.
+	riversharedtest.WaitOrTimeoutN(testutil.PanicTB(), subscribeChan, 1)
 
 	// Periodic jobs can also be configured dynamically after a client has
 	// already started. Added jobs are scheduled for run immediately.

example_unique_job_test.go
@@ -112,7 +112,8 @@ func Example_uniqueJob() {
 
 	// Cheat a little by waiting for the first job to come back so we can
 	// guarantee that this example's output comes out in order.
-	waitForNJobs(subscribeChan, 1)
+	// Wait for jobs to complete. Only needed for purposes of the example test.
+	riversharedtest.WaitOrTimeoutN(testutil.PanicTB(), subscribeChan, 1)
 
 	// Because the job is unique ByArgs, another job for account 2 is allowed.
 	_, err = riverClient.Insert(ctx, ReconcileAccountArgs{AccountID: 2}, nil)
@@ -120,7 +121,8 @@ func Example_uniqueJob() {
 		panic(err)
 	}
 
-	waitForNJobs(subscribeChan, 1)
+	// Wait for jobs to complete. Only needed for purposes of the example test.
+	riversharedtest.WaitOrTimeoutN(testutil.PanicTB(), subscribeChan, 1)
 
 	if err := riverClient.Stop(ctx); err != nil {
 		panic(err)

example_work_func_test.go
@@ -67,7 +67,8 @@ func Example_workFunc() {
 		panic(err)
 	}
 
-	waitForNJobs(subscribeChan, 1)
+	// Wait for jobs to complete. Only needed for purposes of the example test.
+	riversharedtest.WaitOrTimeoutN(testutil.PanicTB(), subscribeChan, 1)
 
 	if err := riverClient.Stop(ctx); err != nil {
 		panic(err)

riverlog/common_test.go
@@ -1,41 +1,0 @@
-package riverlog_test
-
-import (
-	"fmt"
-	"time"
-
-	"github.com/riverqueue/river"
-	"github.com/riverqueue/river/rivershared/riversharedtest"
-	"github.com/riverqueue/river/rivershared/util/sliceutil"
-	"github.com/riverqueue/river/rivertype"
-)
-
-//
-// This file used as a holding place for test helpers for examples so that the
-// helpers aren't included in Godoc and keep each example more succinct.
-//
-
-// Wait on the given subscription channel for numJobs. Times out with a panic if
-// jobs take too long to be received.
-func waitForNJobs(subscribeChan <-chan *river.Event, numJobs int) []*rivertype.JobRow {
-	var (
-		timeout  = riversharedtest.WaitTimeout()
-		deadline = time.Now().Add(timeout)
-		events   = make([]*river.Event, 0, numJobs)
-	)
-
-	for {
-		select {
-		case event := <-subscribeChan:
-			events = append(events, event)
-
-			if len(events) >= numJobs {
-				return sliceutil.Map(events, func(e *river.Event) *rivertype.JobRow { return e.Job })
-			}
-
-		case <-time.After(time.Until(deadline)):
-			panic(fmt.Sprintf("waitForNJobs timed out after waiting %s (received %d job(s), wanted %d)",
-				timeout, len(events), numJobs))
-		}
-	}
-}

riverlog/example_middleware_test.go
@@ -90,9 +90,9 @@ func Example_middleware() {
 	}
 
 	// Wait for job to complete, extract log data out of metadata, and print it.
-	for _, job := range waitForNJobs(subscribeChan, 1) {
+	for _, event := range riversharedtest.WaitOrTimeoutN(testutil.PanicTB(), subscribeChan, 1) {
 		var metadataWithLog metadataWithLog
-		if err := json.Unmarshal(job.Metadata, &metadataWithLog); err != nil {
+		if err := json.Unmarshal(event.Job.Metadata, &metadataWithLog); err != nil {
 			panic(err)
 		}
 		for _, logAttempt := range metadataWithLog.RiverLog {