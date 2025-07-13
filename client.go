7b5843f44461182e9fcab09c6805281833407d13
Blake Gentry <blakesgentry@gmail.com>
2025-07-13 13:57:15 -0500

    add Client.Schema() to return configured schema

--- client.go
+++ client.go
@@ -2567,6 +2567,9 @@ func (c *Client[TTx]) queueUpdate(ctx context.Context, executorTx riverdriver.Ex
 	return queue, controlEvent, nil
 }
 
+// Schema returns the configured schema for the client.
+func (c *Client[TTx]) Schema() string { return c.config.Schema }
+
 // QueueBundle is a bundle for adding additional queues. It's made accessible
 // through Client.Queues.
 type QueueBundle struct {
