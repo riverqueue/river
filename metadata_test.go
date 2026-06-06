5b985d54e6345c0f9b17992d03f0808bfc300793
Brandur <brandur@brandur.org>
2026-06-06 06:50:49 -0700

    Fix small lint problem in `metadata_test.go` (#1278)
    I'm not sure how this snuck through CI originally, but it keeps showing
    up in all my branches. I figure fix it once so we won't have to deal
    with it everywhere else.

--- metadata_test.go
+++ metadata_test.go
@@ -4,8 +4,9 @@ import (
 	"context"
 	"testing"
 
-	"github.com/riverqueue/river/internal/jobexecutor"
 	"github.com/stretchr/testify/require"
+
+	"github.com/riverqueue/river/internal/jobexecutor"
 )
 
 func TestMetadataSet(t *testing.T) {
